fd0c335afa68cd2e146a00f9e9eef51c7ea67fb5
Rin Minase <rin.black.raison.detre@gmail.com>
2018-04-04 22:36:50 +0800

    ANIDB: removed alias in importing footer module

--- src/app/index.components.js
+++ src/app/index.components.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import {default as footer} from "./common/footer/footer.module";
+import footer from "./common/footer/footer.module";
 
 export default angular.module("index.components", [
 	footer.name,
