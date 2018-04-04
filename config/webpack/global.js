3f75a102f277f59f184f218a788e47bb1420f144
Rin Minase <rin.black.raison.detre@gmail.com>
2018-04-04 17:27:30 +0800

    ANIDB: fixed formatting of webpack files

--- config/webpack/global.js
+++ config/webpack/global.js
@@ -13,174 +13,146 @@ var DEVELOPMENT = NODE_ENV === "production" ? false : true;
 var stylesLoader = 'css-loader?root=' + rootPublic + '&sourceMap!postcss-loader!sass-loader?outputStyle=expanded&sourceMap=true&sourceMapContents=true';
 
 module.exports = function (_path) {
-  var rootAssetPath = _path + 'src';
+    var rootAssetPath = _path + 'src';
 
-  var webpackConfig = {
-    // entry points
-    entry: {
-      app: _path + '/src/app/index.bootstrap.js'
-    },
+	var webpackConfig = {
+		// entry points
+		entry: {
+			app: _path + '/src/app/index.bootstrap.js'
+		},
 
-    // output system
-    output: {
-      path: _path + '/dist',
-      filename: '[name].js',
-      publicPath: '/'
-    },
+		// output system
+		output: {
+			path: _path + '/dist',
+			filename: '[name].js',
+			publicPath: '/'
+		},
 
-    // resolves modules
-    resolve: {
-      extensions: ['.js', '.es6', '.jsx', '.scss', '.css'],
-      alias: {
-        _appRoot: path.join(_path, 'src', 'app'),
-        _images: path.join(_path, 'src', 'assets', 'images'),
-        _stylesheets: path.join(_path, 'src', 'assets', 'styles'),
-        _scripts: path.join(_path, 'src', 'assets', 'js')
-      }
-    },
+		// resolves modules
+		resolve: {
+			extensions: ['.js', '.es6', '.jsx', '.scss', '.css'],
+			alias: {
+				_appRoot: path.join(_path, 'src', 'app'),
+				_images: path.join(_path, 'src', 'assets', 'images'),
+				_stylesheets: path.join(_path, 'src', 'assets', 'styles'),
+				_scripts: path.join(_path, 'src', 'assets', 'js')
+			}
+		},
 
-    // modules resolvers
-    module: {
-      rules: [{
-        test: /\.html$/,
-        use: [
-            {
-              loader: 'ngtemplate-loader',
-              options: {
-                relativeTo: path.join(_path, '/src')
-              }
-            },
-            {
-              loader: 'html-loader',
-              options: {
-                attrs: ['img:src', 'img:data-src']
-              }
-            }
-        ]
-      }, {
-        test: /\.js$/,
-        exclude: [
-          path.resolve(_path, "node_modules")
-        ],
-        enforce: 'pre',
-        use: [
-          {
-            loader: 'eslint-loader'
-          }
-        ]
-      }, {
-        test: /\.js$/,
-        exclude: [
-          path.resolve(_path, "node_modules")
-        ],
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              cacheDirectory: false
-            }
-          },
-          {
-            loader: 'baggage-loader?[file].html&[file].css'
-          }
-        ]
-      }, {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader?sourceMap'
-          },
-          {
-            loader: 'postcss-loader'
-          }
-        ]
-      }, {
-        test: /\.(scss|sass)$/,
-        loader: DEVELOPMENT ? ('style-loader!' + stylesLoader) : ExtractTextPlugin.extract({
-          fallbackLoader: "style-loader",
-          loader: stylesLoader
-        })
-      }, {
-        test: /\.(woff2|woff|ttf|eot|svg)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              name: 'assets/fonts/[name]_[hash].[ext]'
-            }
-          }
-        ]
-      }, {
-        test: /\.(jpe?g|png|gif)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              name: 'assets/images/[name]_[hash].[ext]',
-              limit: 10000
-            }
-          }
-        ]
-      }
-      ]
-    },
+		// modules resolvers
+		module: {
+			rules: [{
+				test: /\.html$/,
+				use: [{
+					loader: 'ngtemplate-loader',
+					options: {
+						relativeTo: path.join(_path, '/src')
+					}
+				}, {
+					loader: 'html-loader',
+					options: {
+						attrs: ['img:src', 'img:data-src']
+					}
+				}]
+			}, {
+				test: /\.js$/,
+				exclude: [
+					path.resolve(_path, "node_modules")
+				],
+				enforce: 'pre',
+				use: [
+					{ loader: 'eslint-loader' }
+				]
+			}, {
+				test: /\.js$/,
+				exclude: [
+					path.resolve(_path, "node_modules")
+				],
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							cacheDirectory: false
+						}
+					}, { loader: 'baggage-loader?[dir].html&[dir].css' }
+				]
+			}, {
+				test: /\.css$/,
+				use: [
+					{ loader: 'style-loader' },
+					{ loader: 'css-loader?sourceMap' },
+					{ loader: 'postcss-loader' }
+				]
+			}, {
+				test: /\.(scss|sass)$/,
+				loader: DEVELOPMENT ? ('style-loader!' + stylesLoader) : ExtractTextPlugin.extract({
+					fallbackLoader: "style-loader",
+					loader: stylesLoader
+				})
+			}, {
+				test: /\.(woff2|woff|ttf|eot|svg)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				use: [{
+					loader: 'url-loader',
+					options: {
+						name: 'assets/fonts/[name]_[hash].[ext]'
+					}
+				}]
+			}, {
+				test: /\.(jpe?g|png|gif)$/i,
+				use: [{
+					loader: 'url-loader',
+					options: {
+						name: 'assets/images/[name]_[hash].[ext]',
+						limit: 10000
+					}
+				}]
+			}]
+		},
 
-    // load plugins
-    plugins: [
-      new webpack.LoaderOptionsPlugin({
-        options: {
-          // PostCSS
-          postcss: [autoprefixer({browsers: ['last 5 versions']})],
-        }
-      }),
-       new webpack.ProvidePlugin({
-           
-           $: 'jquery',
-           jQuery: 'jquery',
-           'window.jQuery': 'jquery',
-           'window.jquery': 'jquery',
-           
-           
-           moment: 'moment',
-           'window.moment': 'moment',
-           
-           
-           _: 'lodash',
-           'window._': 'lodash',
-           
-       }),
-      new webpack.DefinePlugin({
-        'NODE_ENV': JSON.stringify(NODE_ENV)
-      }),
-      new webpack.NoEmitOnErrorsPlugin(),
-      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-      new webpack.optimize.AggressiveMergingPlugin({
-        moveToParents: true
-      }),
-      new webpack.optimize.CommonsChunkPlugin({
-        name: 'common',
-        async: true,
-        children: true,
-        minChunks: Infinity
-      }),
-      new Manifest(path.join(_path + '/config', 'manifest.json'), {
-        rootAssetPath: rootAssetPath,
-        ignorePaths: ['.DS_Store']
-      }),
-      new ExtractTextPlugin({
-        filename: 'assets/styles/css/[name]' + (NODE_ENV === 'development' ? '' : '.[chunkhash]') + '.css',
-        allChunks: true
-      }),
-      new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: path.join(_path, 'src', 'tpl-index.ejs')
-      })
-    ]
-  };
+		// load plugins
+		plugins: [
+			new webpack.LoaderOptionsPlugin({
+				options: {
+					// PostCSS
+					postcss: [autoprefixer({browsers: ['last 5 versions']})],
+				}
+			}),
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                'window.jQuery': 'jquery',
+                'window.jquery': 'jquery',
 
-  return webpackConfig;
+                moment: 'moment',
+                'window.moment': 'moment',
 
+                _: 'lodash',
+                'window._': 'lodash',
+            }),
+			new webpack.DefinePlugin({'NODE_ENV': JSON.stringify(NODE_ENV)}),
+			new webpack.NoEmitOnErrorsPlugin(),
+			new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+			new webpack.optimize.AggressiveMergingPlugin({moveToParents: true}),
+			new webpack.optimize.CommonsChunkPlugin({
+				name: 'common',
+				async: true,
+				children: true,
+				minChunks: Infinity
+			}),
+			new Manifest(path.join(_path + '/config', 'manifest.json'), {
+				rootAssetPath: rootAssetPath,
+				ignorePaths: ['.DS_Store']
+			}),
+			new ExtractTextPlugin({
+				filename: 'assets/styles/css/[name]' + (NODE_ENV === 'development' ? '' : '.[chunkhash]') + '.css',
+				allChunks: true
+			}),
+			new HtmlWebpackPlugin({
+				filename: 'index.html',
+				template: path.join(_path, 'src', 'tpl-index.ejs')
+			})
+		]
+	};
+
+	return webpackConfig;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,24 @@
 
 var _ = require('lodash');
 var _configs = {
+    // global section
+    global: require(__dirname + '/config/webpack/global'),
 
-  // global section
-  global: require(__dirname + '/config/webpack/global'),
-
-  // config by enviroments
-  production: require(__dirname + '/config/webpack/environments/production'),
-  development: require(__dirname + '/config/webpack/environments/development')
+    // config by enviroments
+    production: require(__dirname + '/config/webpack/environments/production'),
+    development: require(__dirname + '/config/webpack/environments/development')
 };
 
 var _load = function() {
-  var ENV = process.env.NODE_ENV || 'production';
+    var ENV = process.env.NODE_ENV || 'production';
 
-  console.log('Current Environment: ', ENV);
+    console.log('Current Environment: ', ENV);
 
-  // load config file by environment
-  return _configs && _.merge(
-    _configs.global(__dirname),
-    _configs[ENV](__dirname)
-  );
+    // load config file by environment
+    return _configs && _.merge(
+        _configs.global(__dirname),
+        _configs[ENV](__dirname)
+    );
 };
 
 module.exports = _load();
