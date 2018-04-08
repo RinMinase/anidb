fb8f2d04fc93dc54b8300543f6a617291875b549
Rin Minase <rin.black.raison.detre@gmail.com>
2018-04-09 03:05:40 +0800

    DATABASE: added create, update and retrieve operations in firebase

--- src/app/pages/main/main.controller.js
+++ src/app/pages/main/main.controller.js
@@ -16,5 +16,104 @@ export default class MainController {
 		this.momentVersion = moment.version;
 		this.firebaseVersion = firebase.SDK_VERSION;
 		this.angularLogo = angularLogo;
+
+		const config = {
+			apiKey: "AIzaSyAMevBdhrcoIGlNddR0UMgdgnJUO_GhZc4",
+			authDomain: "rin-anidb.firebaseapp.com",
+			databaseURL: "https://rin-anidb.firebaseio.com",
+			projectId: "rin-anidb",
+			storageBucket: "rin-anidb.appspot.com",
+			messagingSenderId: "669722574117",
+		};
+
+		firebase.initializeApp(config);
+
+		firebase.auth()
+			.signInWithEmailAndPassword(
+				"testing@test.com",
+				"testing"
+			).then(() => {
+				// eslint-disable-next-line
+				console.log("Successfully logged in to firebase!");
+			});
+
+		firebase.auth().onAuthStateChanged((isAuthenticated) => {
+			if (isAuthenticated) {
+				const dataDump = {
+					quality: "4k 2160p",
+					title: "Kimi no Na Wa",
+					episodes: 1,
+					ovas: 0,
+					specials: 0,
+					filesize: 209715200,
+					seasonNumber: 1,
+					firstSeasonTitle: "Kimi no Na Wa",
+					prequel: "",
+					sequel: "",
+					offquel: "",
+					dateFinished: 1523232000,
+					releaseSeason: "Fall",
+					releaseYear: "2013",
+					duration: 7200,
+					encoder: "Coalgirls",
+					variants: "",
+					remarks: "",
+					rating: {
+						audio: 7,
+						enjoyment: 7,
+						graphics: 7,
+						plot: 7,
+					},
+				};
+
+				firebase.database()
+					.ref("anime/" + "1")
+					.set(dataDump)
+					.then(() => {
+						// eslint-disable-next-line
+						console.log("Successfully inserted data in firebase!");
+					});
+
+				firebase.database()
+					.ref("/anime/1")
+					.once("value")
+					.then((data) => {
+						// eslint-disable-next-line
+						console.log(data.val());
+
+						// eslint-disable-next-line
+						console.log("Successfully retrieved data in firebase!");
+					});
+
+				const updateDataDump = {
+					quality: "FHD 1080p",
+				};
+
+				firebase.database()
+					.ref("/anime/1")
+					.update(updateDataDump);
+
+				firebase.database()
+					.ref("/anime/1")
+					.once("value")
+					.then((data) => {
+						// eslint-disable-next-line
+						console.log(data.val());
+
+						// eslint-disable-next-line
+						console.log("Successfully updated data in firebase!");
+					});
+
+				firebase.auth()
+					.signOut()
+					.then(() => {
+						// eslint-disable-next-line
+						console.log("Successfully logged out in firebase!");
+					});
+			} else {
+				// eslint-disable-next-line
+				console.log("Not logged-in in firebase!");
+			}
+		});
 	}
 }
