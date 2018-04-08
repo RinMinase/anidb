75387dabad25a80251d94c97d2227b7d177e479e
Rin Minase <rin.black.raison.detre@gmail.com>
2018-04-09 03:03:12 +0800

    DATABASE: imported firebase and displayed version number

--- src/app/pages/main/main.controller.js
+++ src/app/pages/main/main.controller.js
@@ -2,6 +2,7 @@
 
 import  _ from "lodash/core";
 import moment from "moment";
+import firebase from "firebase";
 import angularLogo from "_images/angular.png";
 
 export default class MainController {
@@ -13,6 +14,7 @@ export default class MainController {
 	$onInit() {
 		this.lodashVersion = _.VERSION;
 		this.momentVersion = moment.version;
+		this.firebaseVersion = firebase.SDK_VERSION;
 		this.angularLogo = angularLogo;
 	}
 }
