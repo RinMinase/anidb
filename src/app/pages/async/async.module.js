222a065ddc74c9dfbea32765822e57a9d320b43d
Rin Minase <rin.black.raison.detre@gmail.com>
2018-04-15 04:04:20 +0800

    ANIDB: refactored async module

--- src/app/pages/async/async.module.js
+++ src/app/pages/async/async.module.js
@@ -4,5 +4,11 @@ import "./async.scss";
 import AsyncController from "./async.controller.js";
 
 export default angular
-	.module("async", [])
-	.controller("AsyncController", AsyncController);
+	.module(
+		"async",
+		[]
+	)
+	.controller(
+		"AsyncController",
+		AsyncController
+	);
