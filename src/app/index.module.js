4ea77c78db49ec767ddad2a92797eb00b67cb846
Rin Minase <rin.black.raison.detre@gmail.com>
2018-04-15 19:18:34 +0800

    REFACTOR: added index module

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,40 +1,35 @@
-"use strict";
+/* global malarkey:false, moment:false */
 
-import indexComponents from "./index.components";
-import { routeConfig } from "./index.routes";
-import coreModule from "./core/core.module";
-import mainModule from "./pages/main/main.module";
-import { LoginController } from "./pages/login/login.controller";
+/* eslint-disable max-len */
 
-import uiRouter from "@uirouter/angularjs";
+import { config } from "./index.config";
+import { routerConfig } from "./index.route";
+import { runBlock } from "./index.run";
+import { MainController } from "./main/main.controller";
+import { GithubContributorService } from "../app/components/githubContributor/githubContributor.service";
+import { WebDevTecService } from "../app/components/webDevTec/webDevTec.service";
+import { NavbarDirective } from "../app/components/navbar/navbar.directive";
+import { MalarkeyDirective } from "../app/components/malarkey/malarkey.directive";
 
-/* eslint-disable no-inline-comments, line-comment-position */
-export default angular
-	.module(
-		"anidb-angular", [
-			// plugins
-			uiRouter,
-			"ngAnimate",
-			"ngCookies",
-			"ngTouch",
-			"ngSanitize",
-			"ngMessages",
-			"oc.lazyLoad",
+angular.module(
+	"anidbAngular", [
+		"ngAnimate",
+		"ngCookies",
+		"ngSanitize",
+		"ngMessages",
+		"ui.router",
+		"ui.bootstrap",
+		"toastr",
+	])
+	.constant("malarkey", malarkey)
+	.constant("moment", moment)
+	.config(config)
+	.config(routerConfig)
+	.run(runBlock)
+	.service("githubContributor", GithubContributorService)
+	.service("webDevTec", WebDevTecService)
+	.controller("MainController", MainController)
+	.directive("acmeNavbar", NavbarDirective)
+	.directive("acmeMalarkey", MalarkeyDirective);
 
-			coreModule.name,		// core
-			indexComponents.name,	// components
-			mainModule.name,		// pages
-		]
-	)
-	.config(($logProvider, $compileProvider) => {
-		"ngInject";
-
-		$logProvider.debugEnabled(true);
-		if (NODE_ENV === "production") {
-			$logProvider.debugEnabled(false);
-			$compileProvider.debugInfoEnabled(false);
-		}
-	})
-	.config(routeConfig)
-	.controller("LoginController", LoginController);
 /* eslint-enable */
