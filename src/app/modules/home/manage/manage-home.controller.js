8b32997543dbec48e2e275a01380d1828d1bd632
Rin Minase <rin.black.raison.detre@gmail.com>
2018-04-25 01:54:46 +0800

    HOME: added retrieval of data from firebase on logging-in

--- src/app/modules/home/manage/manage-home.controller.js
+++ src/app/modules/home/manage/manage-home.controller.js
@@ -1,19 +1,69 @@
+import firebase from "firebase";
+import moment from "moment";
+import Promise from "bluebird";
+
 export class ManageHomeController {
 	constructor (
 		$log,
 		$scope,
-		$uibModal
+		$uibModal,
+		$window
 	) {
 		"ngInject";
 
 		this.$log = $log;
+		this.$scope = $scope;
 		this.$uibModal = $uibModal;
+		this.$window = $window;
 
 		this.activate();
 	}
 
 	activate() {
-		this.$log.log("testing!!");
+		this.validateUser();
+	}
+
+	validateUser() {
+		const authValidation = () => new Promise((resolve) => {
+			firebase.auth().onAuthStateChanged((isAuthenticated) => {
+				if (isAuthenticated) {
+					dataRetrieve();
+					resolve();
+				} else {
+					this.$window.location.href = "/login";
+				}
+			});
+		});
+
+		const dataRetrieve = () => new Promise((resolve) => {
+			firebase.database()
+				.ref("/anime")
+				.once("value")
+				.then((data) => {
+					this.data = data.val();
+					this.data.shift();
+
+					this.data.map((value) => {
+						const filesize = parseFloat(value.filesize);
+
+						if (filesize === 0) {
+							value.filesize = "-";
+						} else if (filesize < 1073741824) {
+							value.filesize = `${(filesize / 1048576).toFixed(2)} MB`;
+						} else {
+							value.filesize = `${(filesize / 1073741824).toFixed(2)} GB`;
+						}
+
+						value.dateFinished = moment.unix(value.dateFinished)
+							.format("MMM DD, YYYY");
+					});
+
+					this.$scope.$apply();
+					resolve();
+				});
+		});
+
+		authValidation();
 	}
 
 	addTitle() {
