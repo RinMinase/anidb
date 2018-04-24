21d179ac75a4d17b4e9dd093b901d55f64db0485
Rin Minase <rin.black.raison.detre@gmail.com>
2018-04-25 01:54:00 +0800

    LOGIN: added logging-in to firebase

--- src/app/modules/login/login.controller.js
+++ src/app/modules/login/login.controller.js
@@ -1,14 +1,33 @@
+import firebase from "firebase";
+
 export class LoginController {
-	constructor ($log, $scope) {
+	constructor (
+		$log,
+		$scope,
+		$window
+	) {
 		"ngInject";
 
 		this.$log = $log;
 		this.$scope = $scope;
+		this.$window = $window;
 
 		this.activate();
 	}
 
 	activate() {
 
+	}
+
+	authenticate() {
+		firebase.auth()
+			.signInWithEmailAndPassword(
+				this.$scope.email,
+				this.$scope.password
+			).then(() => {
+				this.$window.location.href = "/";
+			}).catch((error) => {
+				this.$log.error(error.message);
+			});
 	}
 }
