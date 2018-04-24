3998dc7825362c317c253d05c250e0ef45d354f9
Rin Minase <rin.black.raison.detre@gmail.com>
2018-04-25 01:51:53 +0800

    CONFIG: added firebase initial configuration

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 export function config ($logProvider, toastrConfig) {
 	"ngInject";
 	// Enable log
@@ -9,6 +11,17 @@ export function config ($logProvider, toastrConfig) {
 	toastrConfig.positionClass = "toast-top-right";
 	toastrConfig.preventDuplicates = true;
 	toastrConfig.progressBar = true;
+
+	const firebaseConfig = {
+		apiKey: "AIzaSyCVsm1c-nS9qS1fFfFo6JCAb908AfPU6Ag",
+		authDomain: "rin-anidb.firebaseapp.com",
+		databaseURL: "https://rin-anidb.firebaseio.com",
+		projectId: "rin-anidb",
+		storageBucket: "rin-anidb.appspot.com",
+		messagingSenderId: "669722574117",
+	};
+
+	firebase.initializeApp(firebaseConfig);
 }
 
 export function runBlock ($log) {
