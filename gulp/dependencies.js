39616e687a82eda152ed49d5d0e1fbb14e4656d7
Rin Minase <rin.black.raison.detre@gmail.com>
2018-05-02 15:43:22 +0800

    ANIDB: merged fonts with dependencies

--- gulp/dependencies.js
+++ gulp/dependencies.js
@@ -194,3 +194,34 @@ var buildStyles = function() {
 			)
 		);
 };
+
+
+/**
+ * Font Tasks
+ */
+var fontpaths = [
+	"node_modules/font-awesome/fonts/*",
+	"node_modules/bootstrap-sass/assets/fonts/bootstrap/*"
+];
+
+var robotofonts = "node_modules/roboto-fontface/fonts/roboto/*";
+
+gulp.task("fonts_roboto", function () {
+	return gulp.src(robotofonts)
+		.pipe(gulp.dest(path.join(conf.paths.tmp, "/serve/fonts/roboto")));
+});
+
+gulp.task("fonts_roboto:dist", function () {
+	return gulp.src(robotofonts)
+		.pipe(gulp.dest(path.join(conf.paths.dist, "/serve/fonts/roboto")));
+});
+
+gulp.task("fonts", ["fonts_roboto"], function () {
+	return gulp.src(fontpaths)
+		.pipe(gulp.dest(path.join(conf.paths.tmp, "/serve/fonts")));
+});
+
+gulp.task("fonts:dist", ["fonts_roboto:dist"], function () {
+	return gulp.src(fontpaths)
+		.pipe(gulp.dest(path.join(conf.paths.dist, "/fonts")));
+});
