b02fad046817d6e837fa14f325d339a707a31740
Rin Minase <rin.black.raison.detre@gmail.com>
2018-05-02 14:36:39 +0800

    ANIDB: merged styles with dependencies

--- gulp/dependencies.js
+++ gulp/dependencies.js
@@ -130,3 +130,67 @@ gulp.task("scripts", function () {
 gulp.task("scripts:watch", ["scripts"], function (callback) {
 	return webpackWrapper(true, callback);
 });
+
+
+/**
+ * Stylesheet Tasks
+ */
+gulp.task("styles-reload", ["styles"], function() {
+	return buildStyles()
+		.pipe(browserSync.stream());
+});
+
+gulp.task("styles", function() {
+	return buildStyles();
+});
+
+var buildStyles = function() {
+	var sassOptions = {
+		outputStyle: "expanded",
+		precision: 10
+	};
+
+	var injectFiles = gulp.src([
+		path.join(conf.paths.src, "/app/**/*.scss"),
+	], { read: false });
+
+	var injectOptions = {
+		transform: function(filePath) {
+			return "@import \"" + filePath + "\";";
+		},
+		starttag: "// injector",
+		endtag: "// endinjector",
+		addRootSlash: false
+	};
+
+
+	return gulp.src([
+			path.join(conf.paths.src, "/assets/styles/index.scss")
+		])
+		.pipe(
+			$.inject(
+				injectFiles,
+				injectOptions
+			)
+		)
+		.pipe($.sourcemaps.init())
+		.pipe($.sass(sassOptions))
+		.on(
+			"error",
+			conf.errorHandler("Sass")
+		)
+		.pipe($.autoprefixer())
+		.on(
+			"error",
+			conf.errorHandler("Autoprefixer")
+		)
+		.pipe($.sourcemaps.write())
+		.pipe(
+			gulp.dest(
+				path.join(
+					conf.paths.tmp,
+					"/serve/app/"
+				)
+			)
+		);
+};
