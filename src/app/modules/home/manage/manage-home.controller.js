be6888fa4972ecbd0ae99aa49d9bab5bb661bdd9
Rin Minase <rin.black.raison.detre@gmail.com>
2018-05-04 02:37:45 +0800

    ANIDB: used firebase service in data retrieval, removed unused imports

--- src/app/modules/home/manage/manage-home.controller.js
+++ src/app/modules/home/manage/manage-home.controller.js
@@ -1,14 +1,13 @@
-import firebase from "firebase";
 // import Fuse from "fuse.js";
 import moment from "moment";
-import Promise from "bluebird";
 
 export class ManageHomeController {
 	constructor (
 		$log,
 		$scope,
 		$uibModal,
-		$window
+		$window,
+		firebase
 	) {
 		"ngInject";
 
@@ -16,6 +15,7 @@ export class ManageHomeController {
 		this.$scope = $scope;
 		this.$uibModal = $uibModal;
 		this.$window = $window;
+		this.firebase = firebase;
 		this.dataLoaded = false;
 		// this.$scope.search = "";
 
@@ -23,96 +23,46 @@ export class ManageHomeController {
 	}
 
 	activate() {
-		this.validateUser();
+		this.firebase.auth()
+			.then(() => {
+				this.firebase.retrieve()
+					.then((data) => {
+						this.formatData(data);
 
-		// this.$scope.$watch(
-		// 	() => this.$scope.search,
-		// 	() => {
-		//
-		// 		if (this.data) {
-		// 			const fuseOptions = {
-		// 				shouldSort: true,
-		// 				threshold: 0.3,
-		// 				location: 0,
-		// 				distance: 100,
-		// 				maxPatternLength: 64,
-		// 				minMatchCharLength: 0,
-		// 				keys: [
-		// 					"title",
-		// 					// "quality",
-		// 					// "releaseSeason",
-		// 					// "releaseYear",
-		// 					// "encoder",
-		// 					// "variants",
-		// 					// "remarks",
-		// 				],
-		// 			};
-		//
-		// 			this.filteredData = new Fuse(this.data, fuseOptions)
-		// 				.search(this.$scope.search);
-		// 		// } else if (this.data) {
-		// 		// 	this.filteredData = this.data.map((data) => Object.create(data));
-		// 		// 	this.data = angular.copy(this.unsearchedData);
-		// 		}
-		//
-		// 	}
-		// );
+						this.data = data;
+						this.dataLoaded = true;
+						this.$scope.$apply();
+					});
+			}).catch(() => {
+				this.$window.location.href = "/login";
+			});
 	}
 
-	validateUser() {
-		const authValidation = () => new Promise((resolve) => {
-			firebase.auth().onAuthStateChanged((isAuthenticated) => {
-				if (isAuthenticated) {
-					dataRetrieve();
-					resolve();
-				} else {
-					this.$window.location.href = "/login";
-				}
-			});
+	formatData(data) {
+		return data.map((value) => {
+			const filesize = parseFloat(value.filesize);
+
+			if (filesize === 0) {
+				value.filesize = "-";
+			} else if (filesize < 1073741824) {
+				value.filesize = `${(filesize / 1048576).toFixed(2)} MB`;
+			} else {
+				value.filesize = `${(filesize / 1073741824).toFixed(2)} GB`;
+			}
+
+			value.dateFinished = moment.unix(value.dateFinished)
+				.format("MMM DD, YYYY");
+
+			delete value.duration;
+			delete value.firstSeasonTitle;
+			delete value.inhdd;
+			delete value.offquel;
+			delete value.prequel;
+			delete value.rating;
+			delete value.seasonNumber;
+			delete value.sequel;
+			delete value.watchStatus;
 		});
-
-		const dataRetrieve = () => new Promise((resolve) => {
-			firebase.database()
-				.ref("/anime")
-				.once("value")
-				.then((data) => {
-					this.data = data.val();
-
-					this.data.map((value) => {
-						const filesize = parseFloat(value.filesize);
-
-						if (filesize === 0) {
-							value.filesize = "-";
-						} else if (filesize < 1073741824) {
-							value.filesize = `${(filesize / 1048576).toFixed(2)} MB`;
-						} else {
-							value.filesize = `${(filesize / 1073741824).toFixed(2)} GB`;
-						}
-
-						value.dateFinished = moment.unix(value.dateFinished)
-							.format("MMM DD, YYYY");
-
-						delete value.duration;
-						delete value.firstSeasonTitle;
-						delete value.inhdd;
-						delete value.offquel;
-						delete value.prequel;
-						delete value.rating;
-						delete value.seasonNumber;
-						delete value.sequel;
-						delete value.watchStatus;
-					});
-
-					// this.unsearchedData = angular.copy(this.data);
-					// this.filteredData = this.data.map((value) => Object.create(value));
-
-					this.dataLoaded = true;
-					this.$scope.$apply();
-					resolve();
-				});
-		});
-
-		authValidation();
 	}
 
 	addTitle() {
