0325837e932d2df6a2f8f41d364ecd552d50e25c
Rin Minase <rin.black.raison.detre@gmail.com>
2018-05-08 00:16:31 +0800

    DOWNLOAD: added temporary code for module for porting

---
+++ src/app/modules/download/manage/manage-download.controller.js
@@ -0,0 +1,44 @@
+import _ from "lodash";
+
+export class ManageDownloadController {
+	constructor(
+		$log,
+		$scope,
+		firebase
+	) {
+		"ngInject";
+
+		_.extend(this, {
+			$log,
+			$scope,
+			firebase,
+			data: {},
+			dataLoaded: false,
+		});
+
+		this.activate();
+	}
+
+	activate() {
+		this.firebase.auth()
+			.then(() => {
+				this.firebase.retrieve()
+					.then((data) => {
+						this.data = this.formatData(data);
+						this.dataLoaded = true;
+						this.$scope.$apply();
+					});
+			}).catch(() => {
+				this.$window.location.href = "/login";
+			});
+	}
+
+	formatData(data) {
+		data.map((value) => {
+			if (value.watchStatus < 2) {
+				return;
+			}
+		});
+
+	}
+}
