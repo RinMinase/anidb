b4bbb2d19ca55d60ba7ca636db9f23c4eb468561
Rin Minase <rin.black.raison.detre@gmail.com>
2018-05-07 21:39:59 +0800

    ABOUT: used new implementation of firebase service

--- src/app/modules/about/about.controller.js
+++ src/app/modules/about/about.controller.js
@@ -10,6 +10,7 @@ export class AboutController {
 			$scope,
 			$window,
 			firebase,
+			data: {},
 			dataLoaded: false,
 		});
 
@@ -17,15 +18,11 @@ export class AboutController {
 	}
 
 	activate() {
-		// this.validateUser();
-
 		this.firebase.auth()
 			.then(() => {
 				this.firebase.retrieve()
 					.then((data) => {
 						this.formatData(data);
-
-						this.data = data;
 						this.dataLoaded = true;
 						this.$scope.$apply();
 					});
@@ -35,7 +32,6 @@ export class AboutController {
 	}
 
 	formatData(data) {
-		this.data = {};
 		this.data.quality = {
 			uhd: 0,
 			fhd: 0,
@@ -65,9 +61,15 @@ export class AboutController {
 			}
 
 			switch (value.quality) {
-				case "4K 2160p": this.data.quality.uhd++; break;
-				case "FHD 1080p": this.data.quality.fhd++; break;
-				case "HD 720p": this.data.quality.hd++; break;
+				case "4K 2160p":
+					this.data.quality.uhd++;
+					break;
+				case "FHD 1080p":
+					this.data.quality.fhd++;
+					break;
+				case "HD 720p":
+					this.data.quality.hd++;
+					break;
 				case "HQ 480p":
 					this.data.quality.hq++;
 					break;
@@ -76,79 +78,14 @@ export class AboutController {
 					break;
 			}
 		});
-	}
 
-	validateUser() {
-		const dataRetrieve = () => new Promise((resolve) => {
-			firebase.database()
-				.ref("/anime")
-				.once("value")
-				.then((data) => {
-					this.rawData = data.val();
+		this.data.totalEpisodes = totalEpisodes;
+		this.data.totalDays = parseInt(totalDuration / 86400);
+		this.data.totalHours = parseInt(totalDuration % 86400 / 3600);
+		this.data.totalMinutes = parseInt(totalDuration % 86400 % 3600 / 60);
+		this.data.totalSeconds = parseInt(totalDuration % 86400 % 3600 % 60);
 
-					this.data = {};
-					this.data.quality = {
-						uhd: 0,
-						fhd: 0,
-						hd: 0,
-						hq: 0,
-						lq: 0,
-					};
-
-					let totalDuration = 0;
-					let totalFilesize = 0;
-					let totalEpisodes = 0;
-
-					this.rawData.map((value) => {
-						totalDuration += parseInt(value.duration);
-						totalFilesize += parseInt(value.filesize);
-
-						if (!isNaN( parseInt(value.episodes) )) {
-							totalEpisodes += parseInt(value.episodes);
-						}
-
-						if (!isNaN( parseInt(value.ovas) )) {
-							totalEpisodes += parseInt(value.ovas);
-						}
-
-						if (!isNaN( parseInt(value.specials) )) {
-							totalEpisodes += parseInt(value.specials);
-						}
-
-						switch (value.quality) {
-							case "4K 2160p":
-								this.data.quality.uhd++;
-								break;
-							case "FHD 1080p":
-								this.data.quality.fhd++;
-								break;
-							case "HD 720p":
-								this.data.quality.hd++;
-								break;
-							case "HQ 480p":
-								this.data.quality.hq++;
-								break;
-							case "LQ 360p":
-								this.data.quality.lq++;
-								break;
-						}
-					});
-
-					this.data.totalEpisodes = totalEpisodes;
-					this.data.totalDays = parseInt(totalDuration / 86400);
-					this.data.totalHours = parseInt(totalDuration % 86400 / 3600);
-					this.data.totalMinutes = parseInt(totalDuration % 86400 % 3600 / 60);
-					this.data.totalSeconds = parseInt(totalDuration % 86400 % 3600 % 60);
-
-					this.data.totalFilesizeGB = parseFloat(totalFilesize / 1073741824).toFixed(2);
-					this.data.totalFilesizeTB = parseFloat(totalFilesize / 1099511627776).toFixed(2);
-
-					this.dataLoaded = true;
-					this.$scope.$apply();
-					resolve();
-				});
-		});
-
-		authValidation();
+		this.data.totalFilesizeGB = parseFloat(totalFilesize / 1073741824).toFixed(2);
+		this.data.totalFilesizeTB = parseFloat(totalFilesize / 1099511627776).toFixed(2);
 	}
 }
