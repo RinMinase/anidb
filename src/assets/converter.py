f742421c61fcd2640b460c4f908366d8f5e8c5c6
Rin Minase <rin.black.raison.detre@gmail.com>
2018-05-08 00:18:15 +0800

    ANIDB: updated python script to match new database layout

--- src/assets/converter.py
+++ src/assets/converter.py
@@ -7,50 +7,71 @@ with open("test.csv") as csv_file:
 	reader = csv.reader(csv_file)
 	for row in reader:
 
-		durationHr = int(row[16]) * 3600
-		durationMin = int(row[17]) * 60
-		durationSec = int(row[18])
-		duration = int(durationHr + durationMin + durationSec)
-
-		if row[13] != "0000-00-00":
-			u_date = int(datetime.datetime.strptime(row[13], '%Y-%m-%d').strftime("%s"))
+		if row[2] == "":
+			dl_prio = -1
 		else:
-			u_date = ""
+			dl_prio = int(row[2])
 
-		if row:
-			my_data.append({
-				"watchStatus": int(row[1]),
-				"quality": row[2],
-				"title": row[3],
-				"episodes": int(row[4]),
-				"ovas": int(row[5]),
-				"specials": int(row[6]),
-				"filesize": int(row[7]),
-				"seasonNumber": int(row[8]),
-				"firstSeasonTitle": row[9],
-				"prequel": row[10],
-				"sequel": row[11],
-				"offquel": row[12],
-				"dateFinished": u_date,
-				"releaseSeason": row[14],
-				"releaseYear": row[15],
-				"duration": duration,
-				"encoder": row[19],
-				"variants": row[20],
-				"remarks": row[21],
-				"inhdd": int(row[22]),
-				"rating": {
-					"audio": int(row[23]),
-					"enjoyment": int(row[24]),
-					"graphics": int(row[25]),
-					"plot": int(row[26])
-				}
-			})
+		if int(row[1]) != 0:
 
-			num_rows += 1
+			if row:
+				my_data.append({
+					"watchStatus": int(row[1]),
+					"downloadPriority": dl_prio,
+					"title": row[4],
+					"releaseSeason": row[15],
+					"releaseYear": row[16],
+					"remarks": row[22],
+					"inhdd": int(row[23])
+				})
+
+		else:
+
+			durationHr = int(row[17]) * 3600
+			durationMin = int(row[18]) * 60
+			durationSec = int(row[19])
+			duration = int(durationHr + durationMin + durationSec)
+
+			if row[14] != "0000-00-00":
+				u_date = int(datetime.datetime.strptime(row[14], '%m/%d/%Y').strftime("%s"))
+			else:
+				u_date = ""
+
+			if row:
+				my_data.append({
+					"watchStatus": int(row[1]),
+					"downloadPriority": dl_prio,
+					"quality": row[3],
+					"title": row[4],
+					"episodes": int(row[5]),
+					"ovas": int(row[6]),
+					"specials": int(row[7]),
+					"filesize": int(row[8]),
+					"seasonNumber": int(row[9]),
+					"firstSeasonTitle": row[10],
+					"prequel": row[11],
+					"sequel": row[12],
+					"offquel": row[13],
+					"dateFinished": u_date,
+					"releaseSeason": row[15],
+					"releaseYear": row[16],
+					"duration": duration,
+					"encoder": row[20],
+					"variants": row[21],
+					"remarks": row[22],
+					"inhdd": int(row[23]),
+					"rating": {
+						"audio": int(row[24]),
+						"enjoyment": int(row[25]),
+						"graphics": int(row[26]),
+						"plot": int(row[27])
+					}
+				})
+
+		num_rows += 1
 
 with open("out.json", "w") as out_file:
 	json.dump(my_data, out_file, indent=2)
 
 	print("Finished converting")
-	print("Rows: " + str(num_rows)) #should be 306
+	print("Rows: " + str(num_rows)) #should be 757
