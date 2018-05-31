3657de9c8f3520348eb89d80b04e76606d5eb96f
Rin Minase <rin.black.raison.detre@gmail.com>
2018-05-31 17:44:21 +0800

    HOME: added retrieving of data by id in url parameter

--- src/app/modules/home/view/view-home.controller.js
+++ src/app/modules/home/view/view-home.controller.js
@@ -2,18 +2,32 @@ import _ from "lodash";
 
 export class ViewHomeController {
 	constructor (
-		$log
+		$log,
+		$state,
+		$stateParams,
+		firebase
 	) {
 		"ngInject";
 
 		_.extend(this, {
 			$log,
+			$state,
+			$stateParams,
+			firebase,
 		});
 
 		this.activate();
 	}
 
 	activate() {
-
+		this.firebase.auth()
+			.then(() => {
+				this.firebase.retrieve("anime", this.$stateParams.id)
+					.then((data) => {
+						this.$log.log(data);
+					});
+			}).catch(() => {
+				this.$state.go("login");
+			});
 	}
 }
