aecf07fe5dcfde600a79ba5da15a0cc4b7e5eed7
Rin Minase <rin.black.raison.detre@gmail.com>
2018-06-07 23:10:47 +0800

    ANIDB: updated github api constants

--- src/app/core/constants/github-api.constant.js
+++ src/app/core/constants/github-api.constant.js
@@ -1,9 +1,8 @@
 export function githubConstants() {
-	const githubApi = "https://api.github.com/repos/RinMinase/anidb-angular";
-	const githubApiOld = "https://api.github.com/repos/RinMinase/anidb";
+	const githubApi = "https://api.github.com/repositories/127729588";
 
 	return {
 		commits: `${githubApi}/commits?per_page=20`,
-		issues: `${githubApiOld}/issues?page=1&per_page=100`,
+		issues: `${githubApi}/issues?page=1&per_page=100`,
 	};
 }
