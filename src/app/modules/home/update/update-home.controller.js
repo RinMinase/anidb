200a2caf4702a525f1cb2bc11a2743e6d3991fef
Rin Minase <rin.black.raison.detre@gmail.com>
2018-06-10 14:47:12 +0800

    HOME: added changes from add modal

--- src/app/modules/home/update/update-home.controller.js
+++ src/app/modules/home/update/update-home.controller.js
@@ -2,6 +2,7 @@ export class UpdateHomeController {
 	constructor (
 		$log,
 		$uibModalInstance,
+		firebase,
 		data
 	) {
 		"ngInject";
@@ -9,17 +10,45 @@ export class UpdateHomeController {
 		_.extend(this, {
 			$log,
 			$uibModalInstance,
+			firebase,
 			data,
+			titleList: [],
 		});
 
 		this.activate();
 	}
 
 	activate() {
+		this.firebase.auth()
+			.then(() => {
+				this.firebase.retrieve()
+					.then((data) => {
+						this.data = this.formatData(data);
+						this.dataLoaded = true;
+						this.$scope.$apply();
+					});
+			}).catch(() => {
+				this.$state.go("login");
+			});
+	}
 
+	addOffquel() {
+		if (!this.data.offquel) {
+			this.data.offquel = this.offquelSelection;
+			this.offquelSelection = "";
+		} else {
+			this.data.offquel += `, ${this.offquelSelection}`;
+			this.offquelSelection = "";
+		}
 	}
 
 	cancel() {
 		this.$uibModalInstance.close(false);
+	}
+
+	formatData(data) {
+		data.map((value) => {
+			this.titleList.push(value.title);
+		});
 	}
 }
