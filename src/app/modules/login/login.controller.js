06f712b7c91c849ff9546053362d1e65efa54e44
Rin Minase <rin.black.raison.detre@gmail.com>
2018-06-11 11:12:09 +0800

    LOGIN: used firebase service

--- src/app/modules/login/login.controller.js
+++ src/app/modules/login/login.controller.js
@@ -1,18 +1,20 @@
-import firebase from "firebase/app";
-
-import "firebase/auth";
+import Promise from "bluebird";
 
 export class LoginController {
 	constructor (
 		$log,
 		$scope,
-		$state
+		$state,
+		firebase
 	) {
 		"ngInject";
 
-		this.$log = $log;
-		this.$scope = $scope;
-		this.$state = $state;
+		_.extend(this, {
+			$log,
+			$scope,
+			$state,
+			firebase,
+		});
 
 		this.activate();
 	}
@@ -22,11 +24,8 @@ export class LoginController {
 	}
 
 	authenticate() {
-		firebase.auth()
-			.signInWithEmailAndPassword(
-				this.$scope.email,
-				this.$scope.password
-			).then(() => {
+		Promise.resolve(this.firebase.login(this.email, this.password))
+			.then(() => {
 				this.$state.go("home.manage");
 			}).catch((error) => {
 				this.$log.error(error.message);
