4bb8b9ff76676a90b7121f3305d55f280dc78698
Rin Minase <rin.black.raison.detre@gmail.com>
2018-06-19 21:11:51 +0800

    ABOUT: excluded default merge commits in changelog

--- src/app/modules/about/about.controller.js
+++ src/app/modules/about/about.controller.js
@@ -157,42 +157,44 @@ export class AboutController {
 		this.$http.get(this.GITHUB_API.commits)
 			.then((response) => {
 				response.data.map((data) => {
-					const { date } = data.commit.author;
-					const rawMessage = data.commit.message.split(":");
-					const rawModule = rawMessage[0]
-						.trimStart()
-						.toLowerCase()
-						.replace(new RegExp("_", "g"), " ");
-					const module = (rawModule === "anidb") ? "" : rawModule;
-					const message = rawMessage[1].trimStart();
-					const commitDate = moment(new Date(date)).format("YYYYMMDD");
-					const title = moment(new Date(date)).format("MMM DD, YYYY");
-					const commitData = {
-						date: this._convertDate(date),
-						email: data.commit.author.email,
-						name: data.commit.author.name,
-						message,
-						module,
-						url: data.html_url,
-					};
-
-					if (!this.githubCommits[commitDate]) {
-						this.githubCommits[commitDate] = {
-							fix: [],
-							new: [],
-							improve: [],
-							title,
+					if (!data.commit.message.includes("Merge branch")) {
+						const { date } = data.commit.author;
+						const rawMessage = data.commit.message.split(":");
+						const rawModule = rawMessage[0]
+							.trimStart()
+							.toLowerCase()
+							.replace(new RegExp("_", "g"), " ");
+						const module = (rawModule === "anidb") ? "" : rawModule;
+						const message = rawMessage[1].trimStart();
+						const commitDate = moment(new Date(date)).format("YYYYMMDD");
+						const title = moment(new Date(date)).format("MMM DD, YYYY");
+						const commitData = {
+							date: this._convertDate(date),
+							email: data.commit.author.email,
+							name: data.commit.author.name,
+							message,
+							module,
+							url: data.html_url,
 						};
-					}
 
-					if (message.includes("fixed") || message.includes("removed")) {
-						this.githubCommits[commitDate].fix.push(commitData);
-					} else if (message.includes("added")
-						|| message.includes("functional")
-						|| message.includes("migrated")) {
-						this.githubCommits[commitDate].new.push(commitData);
-					} else {
-						this.githubCommits[commitDate].improve.push(commitData);
+						if (!this.githubCommits[commitDate]) {
+							this.githubCommits[commitDate] = {
+								fix: [],
+								new: [],
+								improve: [],
+								title,
+							};
+						}
+
+						if (message.includes("fixed") || message.includes("removed")) {
+							this.githubCommits[commitDate].fix.push(commitData);
+						} else if (message.includes("added")
+							|| message.includes("functional")
+							|| message.includes("migrated")) {
+							this.githubCommits[commitDate].new.push(commitData);
+						} else {
+							this.githubCommits[commitDate].improve.push(commitData);
+						}
 					}
 				});
 			});
