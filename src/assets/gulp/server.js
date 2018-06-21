b66b65b13f3600c7dd75672072849483d6838bdb
Rin Minase <rin.black.raison.detre@gmail.com>
2018-06-21 23:18:28 +0800

    ANIDB: removed firebase rules in dist folder

--- src/assets/gulp/server.js
+++ src/assets/gulp/server.js
@@ -182,7 +182,8 @@ gulp.task("other", function () {
 
 	return gulp.src([
 		path.join(conf.paths.src, "/**/*"),
-		path.join("!" + conf.paths.src, "/**/*.{html,css,js,scss}")
+		path.join("!" + conf.paths.src, "/**/*.{html,css,js,scss}"),
+		path.join("!" + conf.paths.src, "/assets/firebase/*")
 	])
 	.pipe(fileFilter)
 	.pipe(gulp.dest(path.join(conf.paths.dist, "/")));
