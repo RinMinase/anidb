b44bca795d9cb1ff090f2fb3f0d10c72bffea5a2
Rin Minase <rin.black.raison.detre@gmail.com>
2018-07-01 02:42:54 +0800

    ANDROID: added hook for post bundling

---
+++ src/res/android/hooks/after_build.bat
@@ -0,0 +1,4 @@
+MOVE /Y ..\..\..\..\platforms\android\app\build\outputs\apk\debug\app-debug.apk ..\..\..\..\www\app.apk
+
+RMDIR /S /Q ..\..\..\..\platforms\android\build\android-profile
+RMDIR /S /Q ..\..\..\..\platforms\android\app\build\outputs
