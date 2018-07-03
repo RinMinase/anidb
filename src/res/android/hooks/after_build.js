5506b580d5ba8f8eb35daf9662f14940859884e7
Rin Minase <rin.black.raison.detre@gmail.com>
2018-07-03 23:34:48 +0800

    ANIDB: modified cordova after build hook

--- src/res/android/hooks/after_build.js
+++ src/res/android/hooks/after_build.js
@@ -1,22 +1,24 @@
 /* eslint-disable */
 
-var fs = require("fs");
-var path = require("path")
-var ConfigParser = context.requireCordovaModule("cordova-lib").configparser;
-var config = new ConfigParser("config.xml");
+module.exports = function(context) {
+	var fs = require("fs");
+	var path = require("path")
+	var ConfigParser = context.requireCordovaModule("cordova-lib").configparser;
+	var config = new ConfigParser("config.xml");
 
-var dir = "./build"
+	var dir = "./build"
 
-var apkLocation = "platforms/android/app/build/outputs/apk/debug";
-var apkFilename = "app-debug.apk";
+	var apkLocation = "platforms/android/app/build/outputs/apk/debug";
+	var apkFilename = "app-debug.apk";
 
-var apkOutput = "build";
-var apkOutputFilename = config.id() + "_" + config.version() + ".apk"
+	var apkOutput = "build";
+	var apkOutputFilename = config.packageName() + "_" + config.version() + ".apk"
 
-if (!fs.existsSync(dir)){
-	fs.unlinkSync(dir);
-	fs.mkdirSync(dir);
+	if (!fs.existsSync(dir)){
+		fs.unlinkSync(dir);
+		fs.mkdirSync(dir);
+	}
+
+	fs.createReadStream(path.join(apkLocation, apkFilename))
+		.pipe(fs.createWriteStream(path.join(apkOutput, apkOutputFilename)));
 }
-
-fs.createReadStream(path.join(apkLocation, apkFilename))
-	.pipe(fs.createWriteStream(path.join(apkOutput, apkOutputFilename)));
