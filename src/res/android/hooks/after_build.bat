2078836c67df09c93b8b716ae270d9af034b80f0
Rin Minase <rin.black.raison.detre@gmail.com>
2018-07-03 23:28:48 +0800

    ANIDB: removed batch file hook

--- src/res/android/hooks/after_build.bat
+++
@@ -1,6 +0,0 @@
-@ECHO off
-
-MOVE /Y platforms\android\app\build\outputs\apk\debug\app-debug.apk app.apk
-
-RMDIR /S /Q platforms\android\build\android-profile
-RMDIR /S /Q platforms\android\app\build\outputs
