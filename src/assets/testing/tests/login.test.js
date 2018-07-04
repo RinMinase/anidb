59e6609ca17a5fc9f10dcadecc1eb90e5f224192
Rin Minase <rin.black.raison.detre@gmail.com>
2018-07-04 21:47:42 +0800

    LOGIN_TEST: refactored test script

--- src/assets/testing/tests/login.test.js
+++ src/assets/testing/tests/login.test.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'Login Testing': function(browser) {
+	'Login': function(browser) {
 		browser
 			.url(browser.launchUrl + "login")
 			.waitForElementVisible("body")
@@ -8,11 +8,8 @@ module.exports = {
 			.assert.visible("input[type=password]")
 			.setValue("input[type=email]", "nightwatch@testing.com")
 			.setValue("input[type=password]", "nightwatch")
-			.waitForElementVisible("button", 1000)
-			.click("button")
-			.pause(2000)
-			.url(browser.launchUrl)
-			.pause(2000)
+			.submitForm("form")
+			.assert.urlEquals(browser.launchUrl)
 			.waitForElementVisible("body")
 			.assert.elementPresent("#animeTable")
 			.end();
