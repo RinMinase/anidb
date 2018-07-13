e0e240a1079fe6649abe0c1f20313aea2797af61
Rin Minase <rin.black.raison.detre@gmail.com>
2018-07-14 01:13:55 +0800

    LOGIN_TEST: improved test script, resolved #49

--- src/assets/testing/tests/login.test.js
+++ src/assets/testing/tests/login.test.js
@@ -1,5 +1,33 @@
 module.exports = {
-	'Login': function(browser) {
+	'Login: Spinner Test': function(browser) {
+		browser
+			.url(browser.launchUrl + "login")
+			.waitForElementVisible("body")
+			.assert.title("Rin's Anime Database")
+			.assert.visible("input[type=email]")
+			.assert.visible("input[type=password]")
+			.setValue("input[type=email]", "nightwatch@testing.com")
+			.setValue("input[type=password]", "-")
+			.submitForm("form")
+			.assert.elementPresent(".spinner.eclipse")
+			.end();
+	},
+
+	'Login: Invalid Credentials': function(browser) {
+		browser
+			.url(browser.launchUrl + "login")
+			.waitForElementVisible("body")
+			.assert.title("Rin's Anime Database")
+			.assert.visible("input[type=email]")
+			.assert.visible("input[type=password]")
+			.setValue("input[type=email]", "nightwatch@testing.com")
+			.setValue("input[type=password]", "invalid")
+			.submitForm("form")
+			.assert.elementPresent(".alert.alert-dismissible")
+			.end();
+	},
+
+	'Login: Valid Credentials': function(browser) {
 		browser
 			.url(browser.launchUrl + "login")
 			.waitForElementVisible("body")
