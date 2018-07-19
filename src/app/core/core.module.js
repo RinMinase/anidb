3c0a2ec918a998ec0242b308c0c17fca7a16cd5a
Rin Minase <rin.black.raison.detre@gmail.com>
2018-07-19 23:14:05 +0800

    FUSE_OPTIONS_BUILDER: added service in module

--- src/app/core/core.module.js
+++ src/app/core/core.module.js
@@ -3,6 +3,8 @@ import DirectivesModule from "./directives/directives.module";
 import { FirebaseFactory } from "./services/firebase.factory";
 import { GithubApiFactory } from "./services/github-api.factory";
 
+import { FuseOptionsBuilder } from "./services/builders/fuse-options.builder";
+
 import { githubConstants } from "./constants/github-api.constant.js";
 
 export default angular
@@ -12,5 +14,7 @@ export default angular
 
 	.factory("firebase", FirebaseFactory)
 	.factory("githubApi", GithubApiFactory)
+
+	.factory("fuseOptionsBuilder", FuseOptionsBuilder)
 
 	.constant("GITHUB_API", githubConstants());
