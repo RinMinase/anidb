0d3911dcc4a94841188364d46d98273eadd25b57
Rin Minase <rin.black.raison.detre@gmail.com>
2018-07-19 11:23:45 +0800

    EXPORT: added sample data for excel exporting

---
+++ src/app/modules/export/excel/excel.controller.js
@@ -0,0 +1,28 @@
+import xlsx from "xlsx";
+
+export class ExcelController {
+	constructor () {
+		"ngInject";
+
+		_.extend(this, {});
+
+		this.activate();
+	}
+
+	activate() {
+
+	}
+
+	exportExcel() {
+		const data = [
+			{ "Column 1": "Data 1", "Column 2": 44 },
+			{ "Column 1": "Data 2", "Column 3": "Another Data" },
+		];
+
+		const sheet = xlsx.utils.json_to_sheet(data);
+		const workbook = xlsx.utils.book_new();
+
+		xlsx.utils.book_append_sheet(workbook, sheet, "Data");
+		xlsx.writeFile(workbook, "sheet.xlsx");
+	}
+}
