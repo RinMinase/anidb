309a4396ffcf327bd609b98b2cb6e6a3b70edc2a
Rin Minase <rin.black.raison.detre@gmail.com>
2019-01-02 12:45:23 +0800

    ANIDB: converted scripts gulp file to es6

--- src/assets/gulp/scripts.js
+++ src/assets/gulp/scripts.js
@@ -1,35 +1,30 @@
-"use strict";
+const { join } = require("path");
+const { task, src, dest } = require("gulp");
+const conf = require("../../../gulpfile.js");
 
-var join = require("path").join;
-var log = require("fancy-log");
-var conf = require("../../../gulpfile.js");
-
-var task = require("gulp").task;
-var src = require("gulp").src;
-var dest = require("gulp").dest;
-
-var browserSync = require("browser-sync");
-var dotenv = require('dotenv').config({ path: "./src/assets/.env" });
-var webpack = require("webpack");
-var webpackStream = require("webpack-stream");
-var CompressionPlugin = require("compression-webpack-plugin");
+const log = require("fancy-log");
+const browserSync = require("browser-sync");
+const dotenv = require("dotenv").config({ path: "./src/assets/.env" });
+const webpack = require("webpack");
+const webpackStream = require("webpack-stream");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 function webpackWrapper(watch, callback) {
-	var webpackOptions = {
-		watch: watch,
+	const webpackOptions = {
+		watch,
 		module: {
 			rules: [{
 				test: /\.js$/,
 				enforce: "pre",
 				exclude: /node_modules/,
-				loader: "eslint-loader"
+				loader: "eslint-loader",
 			}, {
 				test: /\.html$/,
 				exclude: /node_modules/,
 				use: [{
 					loader: "ngtemplate-loader",
-					options: { relativeTo: join(conf.paths.root, conf.paths.src) }
-				}, { loader: "html-loader" }]
+					options: { relativeTo: join(conf.paths.root, conf.paths.src) },
+				}, { loader: "html-loader" }],
 			}, {
 				test: /\.js$/,
 				exclude: /node_modules/,
@@ -38,23 +33,23 @@ function webpackWrapper(watch, callback) {
 					options: {
 						ngAnnotate: "ng-annotate-patched",
 						es6: true,
-						explicitOnly: false
-					}
+						explicitOnly: false,
+					},
 				}, {
 					loader: "babel-loader",
-					options: { presets: ["@babel/preset-env"] }
+					options: { presets: ["@babel/preset-env"] },
 				}],
-			}]
+			}],
 		},
 		optimization: {
-			splitChunks: { chunks: "all" }
+			splitChunks: { chunks: "all" },
 		},
 		output: {
 			filename: "index.module.js",
 			path: join(conf.paths.root, conf.paths.tmp, "/serve/app"),
 		},
 		performance: { hints: false },
-		plugins: []
+		plugins: [],
 	};
 
 	if (watch) {
@@ -66,7 +61,7 @@ function webpackWrapper(watch, callback) {
 		webpackOptions.plugins.push(new webpack.DefinePlugin({ "process.env": dotenv.parsed }));
 	}
 
-	var webpackChangeHandler = function(err, stats) {
+	const webpackChangeHandler = function(err, stats) {
 		if (err) {
 			conf.errorHandler("Webpack")(err);
 		}
@@ -79,7 +74,7 @@ function webpackWrapper(watch, callback) {
 				entrypoints: false,
 				hash: false,
 				modules: false,
-				version: false
+				version: false,
 			})
 		);
 
@@ -87,11 +82,11 @@ function webpackWrapper(watch, callback) {
 
 		if (watch) {
 			watch = false;
-			callback();
+			callback(); //eslint-disable-line
 		}
 	};
 
-	return src(conf.paths.src + "/app/index.module.js")
+	return src(`${conf.paths.src}/app/index.module.js`)
 		.pipe(
 			webpackStream(
 				webpackOptions,
@@ -102,10 +97,6 @@ function webpackWrapper(watch, callback) {
 		.pipe(dest(join(conf.paths.tmp, "/serve/app")));
 }
 
-task("scripts", function () {
-	return webpackWrapper(false);
-});
+task("scripts", () => webpackWrapper(false));
 
-task("scripts:watch", function (callback) {
-	return webpackWrapper(true, callback);
-});
+task("scripts:watch", (callback) => webpackWrapper(true, callback));
