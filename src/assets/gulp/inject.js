2a72dd8683423eac3a6522b5207c38fb526bec75
Rin Minase <rin.black.raison.detre@gmail.com>
2019-01-02 09:40:52 +0800

    ANIDB: migrated gulp inject file

--- src/assets/gulp/inject.js
+++ src/assets/gulp/inject.js
@@ -1,9 +1,12 @@
 "use strict";
 
-var fs = require("fs");
-var path = require("path");
-var gulp = require("gulp");
+var join = require("path").join;
 var conf = require("../../../gulpfile.js");
+
+var task = require("gulp").task;
+var series = require("gulp").series;
+var src = require("gulp").src;
+var dest = require("gulp").dest;
 
 var browserSync = require("browser-sync");
 var inject = require("gulp-inject");
@@ -12,24 +15,20 @@ var rename = require('gulp-rename');
 /**
  * Injection Tasks
  */
-gulp.task("inject-reload", ["inject"], function() {
-	browserSync.reload();
-});
-
-gulp.task("inject", function () {
-	var injectStyles = gulp.src([
-		path.join(conf.paths.tmp, "/serve/app/**/*.css"),
-		path.join("!" + conf.paths.tmp, "/serve/app/vendor.css")
+task("inject", function () {
+	var injectStyles = src([
+		join(conf.paths.tmp, "/serve/app/**/*.css"),
+		join("!" + conf.paths.tmp, "/serve/app/vendor.css")
 	], { read: false });
 
-	var injectScripts = gulp.src([
-		path.join(conf.paths.tmp, "/serve/app/**/*.module.js")
+	var injectScripts = src([
+		join(conf.paths.tmp, "/serve/app/**/*.module.js")
 	], { read: false });
 
 	var injectStylesOptions = {
 		ignorePath: [
 			conf.paths.src,
-			path.join(conf.paths.tmp, "/serve")
+			join(conf.paths.tmp, "/serve")
 		],
 		addRootSlash: false
 	};
@@ -37,7 +36,7 @@ gulp.task("inject", function () {
 	var injectScriptsOptions = {
 		ignorePath: [
 			conf.paths.src,
-			path.join(conf.paths.tmp, "/serve")
+			join(conf.paths.tmp, "/serve")
 		],
 		addRootSlash: false,
 		transform: (path) => {
@@ -45,33 +44,37 @@ gulp.task("inject", function () {
 		}
 	};
 
-	return gulp.src(path.join(conf.paths.src, "/index.html"))
+	return src(conf.paths.src + "/index.html")
 		.pipe(inject(injectStyles, injectStylesOptions))
 		.pipe(inject(injectScripts, injectScriptsOptions))
-		.pipe(gulp.dest(path.join(conf.paths.tmp, "/serve")));
+		.pipe(dest(join(conf.paths.tmp, "/serve")));
 });
 
-gulp.task("inject:bundle", ["scripts", "styles"], function () {
-	var injectStyles = gulp.src([
-		path.join(conf.paths.tmp, "/serve/app/**/*.css"),
-		path.join("!" + conf.paths.tmp, "/serve/app/vendor.css")
+task("inject-reload", series("inject", function() {
+	browserSync.reload();
+}));
+
+task("inject:bundle", series("scripts", "styles", function () {
+	var injectStyles = src([
+		join(conf.paths.tmp, "/serve/app/**/*.css"),
+		join("!" + conf.paths.tmp, "/serve/app/vendor.css")
 	], { read: false });
 
-	var injectScripts = gulp.src([
-		path.join(conf.paths.tmp, "/serve/app/**/*.module.js")
+	var injectScripts = src([
+		join(conf.paths.tmp, "/serve/app/**/*.module.js")
 	], { read: false });
 
 	var injectOptions = {
 		ignorePath: [
 			conf.paths.src,
-			path.join(conf.paths.tmp, "/serve")
+			join(conf.paths.tmp, "/serve")
 		],
 		addRootSlash: false
 	};
 
-	return gulp.src(path.join(conf.paths.src, "/index-mobile.html"))
+	return src(conf.paths.src + "/index-mobile.html")
 		.pipe(inject(injectStyles, injectOptions))
 		.pipe(inject(injectScripts, injectOptions))
 		.pipe(rename("index.html"))
-		.pipe(gulp.dest(path.join(conf.paths.tmp, "/serve")));
-});
+		.pipe(dest(join(conf.paths.tmp, "/serve")));
+}));
