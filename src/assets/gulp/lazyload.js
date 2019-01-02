c9ac96d2fe9921116b23dbc623e6b1df1df2362b
Rin Minase <rin.black.raison.detre@gmail.com>
2019-01-02 09:41:02 +0800

    ANIDB: migrated gulp lazyload file

--- src/assets/gulp/lazyload.js
+++ src/assets/gulp/lazyload.js
@@ -1,24 +1,22 @@
 "use strict";
-
-var fs = require("fs");
-var path = require("path");
-var gulp = require("gulp");
-
+;
+var join = require("path").join;
 var conf = require("../../../gulpfile.js");
 
-/**
- * Lazyload Tasks
- */
+var task = require("gulp").task;
+var src = require("gulp").src;
+var dest = require("gulp").dest;
+
 var lazyLoadedModules = [
 	"node_modules/xlsx/dist/xlsx.full.min.js"
 ]
 
-gulp.task("lazyload", function () {
-	return gulp.src(lazyLoadedModules)
-		.pipe(gulp.dest(path.join(conf.paths.tmp, "/serve/dependencies")));
+task("lazyload", function () {
+	return src(lazyLoadedModules)
+		.pipe(dest(join(conf.paths.tmp, "/serve/dependencies")));
 });
 
-gulp.task("lazyload:dist", function () {
-	return gulp.src(lazyLoadedModules)
-		.pipe(gulp.dest(path.join(conf.paths.dist, "/dependencies")));
+task("lazyload:dist", function () {
+	return src(lazyLoadedModules)
+		.pipe(dest(join(conf.paths.dist, "/dependencies")));
 });
