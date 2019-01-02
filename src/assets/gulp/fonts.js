441cca3ebceed09d300575fa1a1b23655cff5219
Rin Minase <rin.black.raison.detre@gmail.com>
2019-01-02 11:43:15 +0800

    ANIDB: converted fonts gulp file to es6

--- src/assets/gulp/fonts.js
+++ src/assets/gulp/fonts.js
@@ -1,25 +1,17 @@
-"use strict";
-
-var fs = require("fs");
-var join = require("path").join;
-var conf = require("../../../gulpfile.js");
-
-var task = require("gulp").task;
-var series = require("gulp").series;
-var src = require("gulp").src;
-var dest = require("gulp").dest;
+const fs = require("fs");
+const { join } = require("path");
+const { task, series, src, dest } = require("gulp");
+const conf = require("../../../gulpfile.js");
 
 /**
- * Font Tasks
- *
- * Used *.woff2 formatting
- *    Remove compatibility in IE11 to reduce files in the compiled (dist/tmp) folder
+ * Used *.woff2, removed compatibility in IE11
+ * to reduce files in the compiled (dist/tmp) folder
  */
-var fontpaths = [
+const fontpaths = [
 	"node_modules/font-awesome/fonts/fontawesome-webfont.woff2",
 ];
 
-var robotofonts = [
+const robotofonts = [
 	"node_modules/roboto-fontface/fonts/roboto/Roboto-Regular.woff2",
 	"node_modules/roboto-fontface/fonts/roboto/Roboto-RegularItalic.woff2",
 	"node_modules/roboto-fontface/fonts/roboto/Roboto-Medium.woff2",
@@ -29,7 +21,7 @@ var robotofonts = [
 	"node_modules/roboto-fontface/fonts/roboto/Roboto-Light.woff2"
 ];
 
-task("fonts_roboto", function (done) {
+task("fonts_roboto", (done) => {
 	if (!fs.existsSync(join(conf.paths.tmp, "/serve/fonts/roboto"))) {
 		return src(robotofonts)
 			.pipe(dest(join(conf.paths.tmp, "/serve/fonts/roboto")));
@@ -38,12 +30,7 @@ task("fonts_roboto", function (done) {
 	done();
 });
 
-task("fonts_roboto:dist", function () {
-	return src(robotofonts)
-		.pipe(dest(join(conf.paths.dist, "/fonts/roboto")));
-});
-
-task("fonts", series("fonts_roboto", function (done) {
+task("fonts", series("fonts_roboto", (done) => {
 	if (!fs.existsSync(join(conf.paths.tmp, "/serve/fonts/fontawesome-webfont.woff2"))) {
 		return src(fontpaths)
 			.pipe(dest(join(conf.paths.tmp, "/serve/fonts")));
@@ -52,7 +39,12 @@ task("fonts", series("fonts_roboto", function (done) {
 	done();
 }));
 
-task("fonts:dist", series("fonts_roboto:dist", function () {
-	return src(fontpaths)
-		.pipe(dest(join(conf.paths.dist, "/fonts")));
-}));
+task("fonts_roboto:dist", () =>
+	src(robotofonts)
+		.pipe(dest(join(conf.paths.dist, "/fonts/roboto")))
+);
+
+task("fonts:dist", series("fonts_roboto:dist", () =>
+	src(fontpaths)
+		.pipe(dest(join(conf.paths.dist, "/fonts")))
+));
