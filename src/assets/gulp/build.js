8dd3f3d9bc29200354b085d2786af6dfa32ac1d4
Rin Minase <rin.black.raison.detre@gmail.com>
2019-01-02 11:36:49 +0800

    ANIDB: converted gulp build file to es6

--- src/assets/gulp/build.js
+++ src/assets/gulp/build.js
@@ -1,40 +1,36 @@
-"use strict";
+const { join } = require("path");
+const { task, src, dest } = require("gulp");
+const conf = require("../../../gulpfile.js");
 
-var join = require("path").join;
-var conf = require("../../../gulpfile.js");
-
-var task = require("gulp").task;
-var src = require("gulp").src;
-var dest = require("gulp").dest;
-
-var angularTemplatecache = require("gulp-angular-templatecache");
-var filter = require("gulp-filter");
-var htmlmin = require("gulp-htmlmin");
-var inject = require("gulp-inject");
-var rev = require("gulp-rev");
-var revReplace = require("gulp-rev-replace");
-var size = require("gulp-size");
-var useref = require("gulp-useref");
+const angularTemplatecache = require("gulp-angular-templatecache");
+const filter = require("gulp-filter");
+const htmlmin = require("gulp-htmlmin");
+const inject = require("gulp-inject");
+const rev = require("gulp-rev");
+const revReplace = require("gulp-rev-replace");
+const size = require("gulp-size");
+const useref = require("gulp-useref");
 
 /**
  * Build Tasks
  */
-task("html", function () {
-	var partialsInjectFile = src(
+task("html", () => {
+	const partialsInjectFile = src(
 		join(conf.paths.tmp, "/partials/templateCacheHtml.js"),
 		{ read: false }
 	);
-	var partialsInjectOptions = {
+
+	const partialsInjectOptions = {
 		starttag: "<!-- inject:partials -->",
 		ignorePath: join(conf.paths.tmp, "/partials"),
-		addRootSlash: false
+		addRootSlash: false,
 	};
 
-	var htmlFilter = filter("*.html", { restore: true });
-	var jsFilter = filter("**/*.js", { restore: true });
-	var cssFilter = filter("**/*.css", { restore: true });
+	const htmlFilter = filter("*.html", { restore: true });
+	const jsFilter = filter("**/*.js", { restore: true });
+	const cssFilter = filter("**/*.css", { restore: true });
 
-	return src(conf.paths.tmp + "/serve/*.html")
+	return src(`${conf.paths.tmp}/serve/*.html`)
 		.pipe(inject(partialsInjectFile, partialsInjectOptions))
 		.pipe(useref())
 		.pipe(jsFilter)
@@ -49,60 +45,52 @@ task("html", function () {
 		.pipe(dest(join(conf.paths.dist, "/")))
 		.pipe(size({
 			title: join(conf.paths.dist, "/"),
-			showFiles: true
+			showFiles: true,
 		}));
 });
 
-task("partials", function () {
+task("partials", () => {
 	return src([
-		conf.paths.src + "/app/**/*.html",
-		conf.paths.tmp + "/serve/app/**/*.html"
-	])
-	.pipe(
+		`${conf.paths.src}/app/**/*.html`,
+		`${conf.paths.tmp}/serve/app/**/*.html`,
+	]).pipe(
 		htmlmin({
 			removeComments: true,
 			removeEmptyAttributes: true,
 			removeAttributeQuotes: true,
 			collapseBooleanAttributes: true,
-			collapseWhitespace: true
+			collapseWhitespace: true,
 		})
-	)
-	.pipe(
+	).pipe(
 		angularTemplatecache(
 			"templateCacheHtml.js", {
 				module: "anidbAngular",
-				root: "app"
+				root: "app",
 			}
 		)
-	)
-	.pipe(dest(conf.paths.tmp + "/partials/"));
+	).pipe(dest(`${conf.paths.tmp}/partials/`));
 });
 
-task("other", function () {
-	var fileFilter = filter(function (file) {
-		return file.stat.isFile();
-	});
+task("other", () => {
+	const fileFilter = filter((file) => file.stat.isFile());
 
 	return src([
-		conf.paths.src + "/**/*",
-		"!" + conf.paths.src + "/**/*.{html,css,js,scss}",
-		"!" + conf.paths.src + "/assets/firebase/*",
-		"!" + conf.paths.src + "/assets/testing/*",
-		"!" + conf.paths.src + "/assets/robots.txt",
-		"!" + conf.paths.src + "/res/**/*"
-	])
-	.pipe(fileFilter)
-	.pipe(dest(join(conf.paths.dist, "/")));
-});
-
-task("robots", function (done) {
-	src(conf.paths.src + "/assets/robots.txt")
+		`${conf.paths.src}/**/*`,
+		`!${conf.paths.src}/**/*.{html,css,js,scss}`,
+		`!${conf.paths.src}/assets/firebase/*`,
+		`!${conf.paths.src}/assets/testing/*`,
+		`!${conf.paths.src}/assets/robots.txt`,
+		`!${conf.paths.src}/res/**/*`,
+	]).pipe(fileFilter)
 		.pipe(dest(join(conf.paths.dist, "/")));
-
-	done();
 });
 
-task("relocate", function () {
-	src(conf.paths.dist + "/**/*")
-		.pipe(dest(conf.paths.www));
-});
+task("robots", () =>
+	src(`${conf.paths.src}/assets/robots.txt`)
+		.pipe(dest(join(conf.paths.dist, "/")))
+);
+
+task("relocate", () =>
+	src(`${conf.paths.dist}/**/*`)
+		.pipe(dest(conf.paths.www))
+);
