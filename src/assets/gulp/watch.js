c4df60eb78e6fed8d86ee7f5384987c73513a3ed
Rin Minase <rin.black.raison.detre@gmail.com>
2019-01-05 08:14:24 +0800

    ANIDB: optimized watch task

--- src/assets/gulp/watch.js
+++ src/assets/gulp/watch.js
@@ -1,21 +1,7 @@
 const { task, watch, series } = require("gulp");
 const conf = require("../../../gulpfile.js");
-const browserSync = require("browser-sync");
 
 task("watch", () => {
-	watch(`${conf.paths.src}/*.html`, series("styles-reload"));
-
-	watch([
-		`${conf.paths.src}/app/**/*.css`,
-		`${conf.paths.src}/app/**/*.scss`,
-		`${conf.paths.src}/assets/styles/*.css`,
-		`${conf.paths.src}/assets/styles/*.scss`,
-	], series("styles-reload"));
-
-	watch([
-		`${conf.paths.src}/app/**/*.html`,
-		`${conf.paths.src}/assets/index.html`,
-	], (event) => {
-		browserSync.reload(event.path);
-	});
+	watch(`${conf.paths.src}/**/*.html`, series("inject-reload"));
+	watch(`${conf.paths.src}/**/*.css`, series("styles-reload"));
 });
