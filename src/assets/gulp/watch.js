237817112b804fe281d302dac933a66fa0094296
Rin Minase <rin.black.raison.detre@gmail.com>
2019-01-05 08:07:04 +0800

    ANIDB: fixed watch gulp task

--- src/assets/gulp/watch.js
+++ src/assets/gulp/watch.js
@@ -1,35 +1,21 @@
-const { join } = require("path");
-const { task, watch, start } = require("gulp");
+const { task, watch, series } = require("gulp");
 const conf = require("../../../gulpfile.js");
 const browserSync = require("browser-sync");
 
-function isOnlyChange(event) {
-	return event.type === "changed";
-}
-
-task("watch", (done) => {
-	watch(
-		join(conf.paths.src, "/*.html"),
-		() => {
-			start("inject-reload");
-		}
-	);
+task("watch", () => {
+	watch(`${conf.paths.src}/*.html`, series("styles-reload"));
 
 	watch([
-		join(conf.paths.src, "/app/**/*.css"),
-		join(conf.paths.src, "/app/**/*.scss"),
-		join(conf.paths.src, "/assets/styles/*.css"),
-		join(conf.paths.src, "/assets/styles/*.scss"),
-	], (event) => {
-		isOnlyChange(event) ? start("styles-reload") : start("inject-reload");
-	});
+		`${conf.paths.src}/app/**/*.css`,
+		`${conf.paths.src}/app/**/*.scss`,
+		`${conf.paths.src}/assets/styles/*.css`,
+		`${conf.paths.src}/assets/styles/*.scss`,
+	], series("styles-reload"));
 
 	watch([
-		join(conf.paths.src, "/app/**/*.html"),
-		join(conf.paths.src, "/assets/index.html"),
+		`${conf.paths.src}/app/**/*.html`,
+		`${conf.paths.src}/assets/index.html`,
 	], (event) => {
 		browserSync.reload(event.path);
 	});
-
-	done();
 });
