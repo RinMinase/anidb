fbc6409167e67c51352278938a7cebcb69434987
Rin Minase <rin.black.raison.detre@gmail.com>
2019-01-06 08:33:57 +0800

    ANIDB: remove usage of join in inject task

--- src/assets/gulp/inject.js
+++ src/assets/gulp/inject.js
@@ -1,4 +1,3 @@
-const { join } = require("path");
 const { task, series, src, dest } = require("gulp");
 const conf = require("../../../gulpfile.js");
 
@@ -6,66 +5,55 @@ const browserSync = require("browser-sync");
 const inject = require("gulp-inject");
 const rename = require("gulp-rename");
 
-task("inject", () => {
-	const injectStyles = src([
-		join(conf.paths.tmp, "/serve/app/**/*.css"),
-		join(`!${conf.paths.tmp}`, "/serve/app/vendor.css")
-	], { read: false });
+task("inject-reload", series("inject", () => {
+	browserSync.reload();
+}));
 
-	const injectScripts = src([
-		join(conf.paths.tmp, "/serve/app/**/*.module.js")
+task("inject", () => {
+	const injectScripts = src(`${conf.paths.tmp}/serve/app/**/*.module.js`, { read: false });
+	const injectStyles = src([
+		`${conf.paths.tmp}/serve/app/**/*.css`,
+		`!${conf.paths.tmp}/serve/app/vendor.css`,
 	], { read: false });
 
 	const injectStylesOptions = {
-		ignorePath: [
-			conf.paths.src,
-			join(conf.paths.tmp, "/serve")
-		],
+		ignorePath: [ conf.paths.src, `${conf.paths.tmp}/serve`],
 		addRootSlash: false,
 	};
 
 	const injectScriptsOptions = {
-		ignorePath: [
-			conf.paths.src,
-			join(conf.paths.tmp, "/serve")
-		],
+		ignorePath: [ conf.paths.src, `${conf.paths.tmp}/serve` ],
 		addRootSlash: false,
-		transform: (path) => {
-			return `<script src='${path}' defer></script>`;
-		}
+		transform: (path) => `<script src='${path}' defer></script>`,
 	};
 
 	return src(`${conf.paths.src}/index.html`)
 		.pipe(inject(injectStyles, injectStylesOptions))
 		.pipe(inject(injectScripts, injectScriptsOptions))
-		.pipe(dest(join(conf.paths.tmp, "/serve")));
+		.pipe(dest(`${conf.paths.tmp}/serve`));
 });
 
-task("inject-reload", series("inject", () => {
-	browserSync.reload();
-}));
-
 task("inject:bundle", series("scripts", "styles", () => {
+	const injectScripts = src(`${conf.paths.tmp}/serve/app/**/*.module.js`, { read: false });
 	const injectStyles = src([
-		join(conf.paths.tmp, "/serve/app/**/*.css"),
-		join(`!${conf.paths.tmp}`, "/serve/app/vendor.css")
+		`${conf.paths.tmp}/serve/app/**/*.css`,
+		`!${conf.paths.tmp}/serve/app/vendor.css`,
 	], { read: false });
 
-	const injectScripts = src([
-		join(conf.paths.tmp, "/serve/app/**/*.module.js")
-	], { read: false });
-
-	const injectOptions = {
-		ignorePath: [
-			conf.paths.src,
-			join(conf.paths.tmp, "/serve")
-		],
+	const injectStylesOptions = {
+		ignorePath: [ conf.paths.src, `${conf.paths.tmp}/serve`],
 		addRootSlash: false,
 	};
 
+	const injectScriptsOptions = {
+		ignorePath: [ conf.paths.src, `${conf.paths.tmp}/serve` ],
+		addRootSlash: false,
+		transform: (path) => `<script src='${path}' defer></script>`,
+	};
+
 	return src(`${conf.paths.src}/index-mobile.html`)
-		.pipe(inject(injectStyles, injectOptions))
-		.pipe(inject(injectScripts, injectOptions))
+		.pipe(inject(injectStyles, injectStylesOptions))
+		.pipe(inject(injectScripts, injectScriptsOptions))
 		.pipe(rename("index.html"))
-		.pipe(dest(join(conf.paths.tmp, "/serve")));
+		.pipe(dest(`${conf.paths.tmp}/serve`));
 }));
