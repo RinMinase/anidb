7509139c1bfd5ef83c6b62fa807ab07b3d38b5c7
Rin Minase <rin.black.raison.detre@gmail.com>
2019-01-07 17:34:51 +0800

    JIKAN: added factory to module

--- src/app/core/core.module.js
+++ src/app/core/core.module.js
@@ -2,18 +2,18 @@ import DirectivesModule from "./directives/directives.module";
 
 import { FirebaseFactory } from "./services/firebase.factory";
 import { GithubApiFactory } from "./services/github-api.factory";
+import { JikanApiFactory } from "./services/firebase-api.factory";
 
 import { FuseOptionsBuilder } from "./services/builders/fuse-options.builder";
 
 import { githubConstants } from "./constants/github-api.constant.js";
 
 export default angular
-	.module("core", [
-		DirectivesModule.name,
-	])
+	.module("core", [ DirectivesModule.name ])
 
 	.factory("firebase", FirebaseFactory)
 	.factory("githubApi", GithubApiFactory)
+	.factory("jikanApi", JikanApiFactory)
 
 	.factory("fuseOptionsBuilder", FuseOptionsBuilder)
 
