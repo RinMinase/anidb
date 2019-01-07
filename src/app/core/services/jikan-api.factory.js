892ac8f04d01d56b891f5addbad9d215833e2e4d
Rin Minase <rin.black.raison.detre@gmail.com>
2019-01-07 17:41:40 +0800

    JIKAN: added search api

--- src/app/core/services/jikan-api.factory.js
+++ src/app/core/services/jikan-api.factory.js
@@ -6,12 +6,18 @@ export function JikanApiFactory() {
 	const jikanApi = "https://api.jikan.moe/v3";
 	const factory = {
 		getData,
+		searchData,
 	}
 
 	return factory;
 
 	function getData(id) {
 		return Promise.resolve(`${jikanApi}/anime${id}`)
+			.catch((error) => Promise.reject(error));
+	}
+
+	function searchData(query, limit = 1) {
+		return Promise.resolve(`${jikanApi}/search/anime?q=${encodeURI(query)}&limit=${limit}`)
 			.catch((error) => Promise.reject(error));
 	}
 }
