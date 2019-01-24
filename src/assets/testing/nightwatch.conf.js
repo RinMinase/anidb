6976b6630733e7f59f338f75a80ca559c84bd25a
Rin Minase <rin.black.raison.detre@gmail.com>
2019-01-24 22:49:35 +0800

    TESTING: added log supressions on arguments

--- src/assets/testing/nightwatch.conf.js
+++ src/assets/testing/nightwatch.conf.js
@@ -8,7 +8,11 @@ log(`URL: ${process.env.TEST_URL}`);
 log(`Username: ${process.env.TEST_USERNAME}`);
 log(`Headless: ${process.env.TEST_HEADLESS || "false"}\n`);
 
-const args = [];
+const args = [
+	"--log-level=3",
+	"--disable-extensions",
+	"--silent",
+];
 
 if (process.env.TEST_HEADLESS === "true") {
 	args.push("--proxy-server='direct://'");
@@ -16,26 +20,26 @@ if (process.env.TEST_HEADLESS === "true") {
 	args.push("--headless");
 }
 
-/* eslint-disable quote-props */
+/* eslint-disable camelcase */
 module.exports = {
-	"src_folders": ["src/assets/testing/tests"],
-	"output_folder": false,
-	"selenium": { "start_process": false },
-	"globals_path": "./chromedriver.conf.js",
-	"test_settings": {
-		"default": {
-			"selenium_host": "127.0.0.1",
-			"selenium_port": 9515,
-			"silent": true,
-			"launch_url" : process.env.TEST_URL || "http://localhost:3000",
-			"globals": {
-				"waitForConditionTimeout": 5000,
-				"retryAssertionTimeout": 20000,
+	src_folders: ["src/assets/testing/tests"],
+	output_folder: false,
+	selenium: { start_process: false },
+	globals_path: "./chromedriver.conf.js",
+	test_settings: {
+		default: {
+			selenium_host: "127.0.0.1",
+			selenium_port: 9515,
+			silent: true,
+			launch_url : process.env.TEST_URL || "http://localhost:3000",
+			globals: {
+				waitForConditionTimeout: 5000,
+				retryAssertionTimeout: 20000,
 			},
-			"desiredCapabilities": {
-				"browserName": "chrome",
-				"javascriptEnabled": true,
-				"chromeOptions": { args },
+			desiredCapabilities: {
+				browserName: "chrome",
+				javascriptEnabled: true,
+				chromeOptions: { args },
 			},
 		},
 	},
