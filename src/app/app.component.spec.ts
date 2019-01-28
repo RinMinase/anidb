951476619e4a6610425a394b653bd375d6a4be7d
Rin Minase <rin.black.raison.detre@gmail.com>
2019-01-28 11:40:18 +0800

    TRANSITION: modified project name in code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'anidb-ngx'`, () => {
+  it(`should have as title 'anidb'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('anidb-ngx');
+    expect(app.title).toEqual('anidb');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to anidb-ngx!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to anidb!');
   });
 });
