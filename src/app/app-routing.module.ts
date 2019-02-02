98657231ab2921a98c4539085fd69ef76f7cf08a
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-02 16:37:00 +0800

    TRANSITION: added lazy loaded module to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [{
+	path: "lazy-test-module",
+	loadChildren: "./lazy-test-module/lazy-test-module.module#LazyTestModuleModule",
+}, {
+	path: "",
+	pathMatch: "full",
+	redirectTo: "",
+}];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
