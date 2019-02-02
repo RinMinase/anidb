84a71eb9b383c3aea432225e6d839f03e117b557
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-02 16:46:50 +0800

    TRANSITION: merged main routing module to main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { TestModuleModule } from "./test-module/test-module.module";
+import { Routes, RouterModule } from "@angular/router";
+
+const routes: Routes = [{
+	path: "lazy-test-module",
+	loadChildren: "./lazy-test-module/lazy-test-module.module#LazyTestModuleModule",
+}, {
+	path: "",
+	pathMatch: "full",
+	redirectTo: "",
+}];
 
 @NgModule({
 	declarations: [
@@ -11,7 +20,8 @@ import { TestModuleModule } from "./test-module/test-module.module";
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule,
+		RouterModule.forRoot(routes),
+
 		TestModuleModule,
 	],
 	providers: [],
