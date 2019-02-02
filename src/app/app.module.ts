86c9b9188ac32b03615c3b9d7dc6264a6645ec75
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-03 01:12:04 +0800

    TRANSITION: remove unused test module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { Routes, RouterModule } from "@angular/router";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppComponent } from "./app.component";
-import { TestModuleModule } from "./test-module/test-module.module";
 import { LoginModule } from "./login/login.module";
 import { HomeModule } from "./home/home.module";
 import { NavbarComponent } from "./navbar/navbar.component";
@@ -30,7 +29,6 @@ const routes: Routes = [{
 		RouterModule.forRoot(routes),
 		NgbModule,
 
-		TestModuleModule,
 		LoginModule,
 		HomeModule,
 	],
