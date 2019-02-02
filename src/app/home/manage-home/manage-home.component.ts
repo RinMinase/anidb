d90b17de49422f4795c5918c245927c578476744
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-03 01:02:29 +0800

    TRANSITION: added manage component in home module

---
+++ src/app/home/manage-home/manage-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "app-manage-home",
+	templateUrl: "./manage-home.component.html",
+	styleUrls: ["./manage-home.component.scss"],
+})
+export class ManageHomeComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
