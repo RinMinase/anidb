dc7adccaa7f62fe2cfde99f9017b26141cbffd74
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-02 13:52:47 +0800

    TRANSITION: added test module in main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestModuleModule } from './test-module/test-module.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+		AppRoutingModule,
+		TestModuleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
