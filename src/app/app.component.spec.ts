8205f9f6167f0c84fceee165cc61a4d0627a055b
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-03 23:57:11 +0800

    TRANSITION: fixed main component testing script

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed, async } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./core/components/navbar/navbar.component";
+import { FooterComponent } from "./core/components/footer/footer.component";
+import { NgbCollapse } from "@ng-bootstrap/ng-bootstrap";
 
 describe("AppComponent", () => {
 	beforeEach(async(() => {
@@ -10,6 +13,9 @@ describe("AppComponent", () => {
 			],
 			declarations: [
 				AppComponent,
+				NavbarComponent,
+				FooterComponent,
+				NgbCollapse,
 			],
 		}).compileComponents();
 	}));
@@ -18,18 +24,5 @@ describe("AppComponent", () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.debugElement.componentInstance;
 		expect(app).toBeTruthy();
-	});
-
-	it(`should have as title "anidb"`, () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.debugElement.componentInstance;
-		expect(app.title).toEqual("anidb");
-	});
-
-	it("should render title in a h1 tag", () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		fixture.detectChanges();
-		const compiled = fixture.debugElement.nativeElement;
-		expect(compiled.querySelector("h1").textContent).toContain("Welcome to anidb!");
 	});
 });
