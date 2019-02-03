f9f989a962ad39b0e7c7bc690cb0427e08db0216
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-03 23:59:44 +0800

    TRANSITION: fixed links in navbar

--- src/app/core/components/navbar/navbar.module.ts
+++ src/app/core/components/navbar/navbar.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { NavbarComponent } from "./navbar.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
 	declarations: [NavbarComponent],
 	imports: [
 		CommonModule,
+		RouterModule,
 		NgbModule,
 	],
 	exports: [NavbarComponent],
