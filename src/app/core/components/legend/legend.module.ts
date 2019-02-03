378034c7550179ecf0a61a996535d76d052bc237
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-03 23:55:57 +0800

    TRANSITION: added legend module file

---
+++ src/app/core/components/legend/legend.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { LegendComponent } from "./legend.component";
+
+@NgModule({
+	declarations: [LegendComponent],
+	imports: [
+		CommonModule,
+		NgbModule,
+	],
+	exports: [LegendComponent],
+})
+export class LegendModule { }
