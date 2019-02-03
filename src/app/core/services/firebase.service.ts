ba046a4ff91c10edda304ffa90863638f7d07beb
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-03 22:08:51 +0800

    TRANSITION: added functions to firebase service

--- src/app/core/services/firebase.service.ts
+++ src/app/core/services/firebase.service.ts
@@ -28,4 +28,54 @@ export class FirebaseService {
 			});
 		});
 	}
+
+	retrieve(
+		db = "anime",
+		id = "",
+		limit = 0,
+		orderKey = "",
+		orderDesc = false,
+		inhdd = true,
+	) {
+
+		id = (id) ? `/${id}` : "";
+		limit = (!isNaN(limit)) ? limit : 0;
+
+		let orderKeyValid = false;
+		const inHddValue = (inhdd && db === "anime") ? 1 : 0;
+
+		if (orderKey || orderKey === "dateFinished") {
+			orderKeyValid = true;
+		}
+
+		firebase.database().ref(`/${db}${id}`).on("value", () => {});
+
+		if (id === "" && !limit && !orderKey && inHddValue) {
+			return firebase.database()
+				.ref(`/${db}${id}`)
+				.orderByChild("inhdd")
+				.equalTo(1)
+				.once("value")
+				.then((data) => this._objectToArray(data.val()));
+		}
+
+		return Promise.reject();
+	}
+
+	private _objectToArray(data: any) {
+		if (!isNaN(Object.keys(data)[0] as any)
+			&& data.constructor.toString().indexOf("Object") !== -1) {
+
+			const output = [];
+
+			Object.keys(data).map((key, index) => {
+				output[index] = data[key];
+				output[index].id = parseInt(key, 10);
+			});
+
+			return output;
+		}
+
+		return data;
+	}
 }
