13fde507cdd93bf26f0fa93eebcda2942b81cefa
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-03 23:11:17 +0800

    TRANSITION: added legend component to home

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ManageHomeComponent } from "./manage-home/manage-home.component";
 import { RouterModule, Routes } from "@angular/router";
+import { LegendComponent } from "../../core/components/legend/legend.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 const routes: Routes = [{
 	path: "", component: ManageHomeComponent,
 }];
 
 @NgModule({
-	declarations: [ManageHomeComponent],
+	declarations: [
+		ManageHomeComponent,
+		LegendComponent,
+	],
 	imports: [
 		CommonModule,
 		RouterModule.forChild(routes),
+		NgbModule,
 	],
 })
 export class HomeModule { }
