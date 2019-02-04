4a2f08bf445b208ca61ba3442318c02e005154ee
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-05 01:13:03 +0800

    TRANSITION: organized imports

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { ManageHomeComponent } from "./manage-home/manage-home.component";
 import { LegendModule } from "../../core/components/legend/legend.module";
-import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [{
 	path: "", component: ManageHomeComponent,
