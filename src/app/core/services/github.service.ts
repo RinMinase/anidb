e7d5127925af57f221536f374e99991446721d89
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-04 21:00:04 +0800

    TRANSITION: added github api service

---
+++ src/app/core/services/github.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+	providedIn: "root",
+})
+export class GithubService {
+
+	githubApi = "https://api.github.com/repos/RinMinase/anidb";
+	commits = `${this.githubApi}/commits?per_page=20`;
+	issues = `${this.githubApi}/issues?page=1&per_page=100`;
+	branch = "ngx";
+	branchCommits = `${this.commits}&sha=${this.branch}`;
+
+	constructor(
+		private http: HttpClient,
+	) { }
+
+	getCommits(): Observable<any> {
+		return this.http.get(this.branchCommits, { observe: "response" });
+	}
+
+	getIssues(): Observable<any> {
+		return this.http.get(this.issues, { observe: "response" });
+	}
+}
