97554236b641a070f87483c192c1f54cc4db08ba
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-05 21:09:37 +0800

    TRANSITION: added manage component in download

---
+++ src/app/modules/download/manage-download/manage-download.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { ManageDownloadComponent } from "./manage-download.component";
+
+describe("ManageDownloadComponent", () => {
+	// let component: ManageDownloadComponent;
+	// let fixture: ComponentFixture<ManageDownloadComponent>;
+
+	// beforeEach(async(() => {
+	// 	TestBed.configureTestingModule({
+	// 		declarations: [ ManageDownloadComponent ],
+	// 	})
+	// 	.compileComponents();
+	// }));
+
+	// beforeEach(() => {
+	// 	fixture = TestBed.createComponent(ManageDownloadComponent);
+	// 	component = fixture.componentInstance;
+	// 	fixture.detectChanges();
+	// });
+
+	// it("should create", () => {
+	// 	expect(component).toBeTruthy();
+	// });
+});
---
+++ src/app/modules/download/manage-download/manage-download.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-manage-download",
+  templateUrl: "./manage-download.component.html",
+  styleUrls: ["./manage-download.component.scss"],
+})
+export class ManageDownloadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
