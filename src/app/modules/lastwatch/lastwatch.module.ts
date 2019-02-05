54069a078719dbf520a830f981c356e3806649ca
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-05 22:34:03 +0800

    TRANSITION: added routes to lastwatch module

--- src/app/modules/lastwatch/lastwatch.module.ts
+++ src/app/modules/lastwatch/lastwatch.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+
 import { LastwatchComponent } from "./lastwatch.component";
+
+const routes: Routes = [{
+	path: "login", component: LastwatchComponent,
+}];
 
 @NgModule({
 	declarations: [LastwatchComponent],
 	imports: [
 		CommonModule,
+		RouterModule.forChild(routes),
 	],
 })
 export class LastwatchModule { }
