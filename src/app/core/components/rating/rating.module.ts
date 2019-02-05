a82727d207edd8934331c714c7b6673658bc0a96
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-05 16:27:58 +0800

    TRANSITION: added rating module

---
+++ src/app/core/components/rating/rating.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { RatingComponent } from "./rating.component";
+
+@NgModule({
+	declarations: [RatingComponent],
+	imports: [
+		CommonModule,
+	],
+	exports: [RatingComponent],
+})
+export class RatingModule { }
