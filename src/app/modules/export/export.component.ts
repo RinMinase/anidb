bc00bb4f130853ab2edaf3b99015780a00360987
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-05 11:47:56 +0800

    TRANSITION: added export module

---
+++ src/app/modules/export/export.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "app-export",
+	template: "<router-outlet></router-outlet>",
+})
+export class ExportComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
---
+++ src/app/modules/export/export.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+
+import { ExportComponent } from "./export.component";
+import { CsvComponent } from "./csv/csv.component";
+import { ExcelComponent } from "./excel/excel.component";
+
+const routes: Routes = [{
+	path: "",
+	component: ExportComponent,
+	children: [{
+		path: "csv",
+		component: CsvComponent,
+	}, {
+		path: "excel",
+		component: ExcelComponent,
+	}],
+}];
+
+@NgModule({
+	declarations: [ExportComponent, CsvComponent, ExcelComponent],
+	imports: [
+		CommonModule,
+		RouterModule.forChild(routes),
+	],
+})
+export class ExportModule { }
