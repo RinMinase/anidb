5e4d19ddd61d220fb7a2057308765680ad8afe77
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-05 21:08:16 +0800

    TRANSITION: added manage component in summer

---
+++ src/app/modules/summer/manage-summer/manage-summer.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { ManageSummerComponent } from "./manage-summer.component";
+
+describe("ManageSummerComponent", () => {
+	// let component: ManageSummerComponent;
+	// let fixture: ComponentFixture<ManageSummerComponent>;
+
+	// beforeEach(async(() => {
+	// 	TestBed.configureTestingModule({
+	// 		declarations: [ ManageSummerComponent ],
+	// 	})
+	// 	.compileComponents();
+	// }));
+
+	// beforeEach(() => {
+	// 	fixture = TestBed.createComponent(ManageSummerComponent);
+	// 	component = fixture.componentInstance;
+	// 	fixture.detectChanges();
+	// });
+
+	// it("should create", () => {
+	// 	expect(component).toBeTruthy();
+	// });
+});
---
+++ src/app/modules/summer/manage-summer/manage-summer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "app-manage-summer",
+	templateUrl: "./manage-summer.component.html",
+	styleUrls: ["./manage-summer.component.scss"],
+})
+export class ManageSummerComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
