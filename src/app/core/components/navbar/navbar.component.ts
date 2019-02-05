ede2de0156f1b2465171223a2244daeb1815b6c9
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-05 11:51:23 +0800

    TRANSITION: removed entry of stylesheet in components

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from "@angular/core";
 @Component({
 	selector: "app-navbar",
 	templateUrl: "./navbar.component.html",
-	styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent implements OnInit {
 	isNavCollapsed: Boolean = false;
