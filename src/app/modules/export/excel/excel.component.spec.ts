516843535f36731cfc41428ad4a353f7e98ab84c
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-05 11:47:23 +0800

    TRANSITION: added excel export module

---
+++ src/app/modules/export/excel/excel.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { ExcelComponent } from "./excel.component";
+
+describe("ExcelComponent", () => {
+	// let component: ExcelComponent;
+	// let fixture: ComponentFixture<ExcelComponent>;
+
+	// beforeEach(async(() => {
+	// 	TestBed.configureTestingModule({
+	// 		declarations: [ ExcelComponent ],
+	// 	})
+	// 	.compileComponents();
+	// }));
+
+	// beforeEach(() => {
+	// 	fixture = TestBed.createComponent(ExcelComponent);
+	// 	component = fixture.componentInstance;
+	// 	fixture.detectChanges();
+	// });
+
+	// it("should create", () => {
+	// 	expect(component).toBeTruthy();
+	// });
+});
