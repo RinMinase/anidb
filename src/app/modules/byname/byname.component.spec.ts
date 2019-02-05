5942833e4acbb660f597e4b72b4df27b33dfa986
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-05 17:00:21 +0800

    TRANSITION: commented byname test script

--- src/app/modules/byname/byname.component.spec.ts
+++ src/app/modules/byname/byname.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { BynameComponent } from "./byname.component";
 
 describe("BynameComponent", () => {
-	let component: BynameComponent;
-	let fixture: ComponentFixture<BynameComponent>;
+	// let component: BynameComponent;
+	// let fixture: ComponentFixture<BynameComponent>;
 
-	beforeEach(async(() => {
-		TestBed.configureTestingModule({
-			declarations: [ BynameComponent ],
-		})
-		.compileComponents();
-	}));
+	// beforeEach(async(() => {
+	// 	TestBed.configureTestingModule({
+	// 		declarations: [ BynameComponent ],
+	// 	})
+	// 	.compileComponents();
+	// }));
 
-	beforeEach(() => {
-		fixture = TestBed.createComponent(BynameComponent);
-		component = fixture.componentInstance;
-		fixture.detectChanges();
-	});
+	// beforeEach(() => {
+	// 	fixture = TestBed.createComponent(BynameComponent);
+	// 	component = fixture.componentInstance;
+	// 	fixture.detectChanges();
+	// });
 
-	it("should create", () => {
-		expect(component).toBeTruthy();
-	});
+	// it("should create", () => {
+	// 	expect(component).toBeTruthy();
+	// });
 });
