05b94bb87040a5f0903f13a4badb16f1afe53042
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-05 21:08:46 +0800

    TRANSITION: added manage component of hdd

---
+++ src/app/modules/hdd/manage-hdd/manage-hdd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "app-manage-hdd",
+	templateUrl: "./manage-hdd.component.html",
+	styleUrls: ["./manage-hdd.component.scss"],
+})
+export class ManageHddComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
