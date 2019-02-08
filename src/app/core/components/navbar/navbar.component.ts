7caba851d25b7c9780784fbf9e5681f79bb94d4d
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-08 23:36:23 +0800

    TRANSITION: added logout functionality to navbar

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+
+import { FirebaseService } from "../../services/firebase.service";
 
 @Component({
 	selector: "app-navbar",
@@ -7,8 +10,16 @@ import { Component, OnInit } from "@angular/core";
 export class NavbarComponent implements OnInit {
 	isNavCollapsed: Boolean = false;
 
-	constructor() { }
+	constructor(
+		private router: Router,
+		private firebase: FirebaseService,
+	) { }
 
 	ngOnInit() {
+	}
+
+	logout() {
+		this.firebase.logout()
+			.then(() => this.router.navigateByUrl("/login"));
 	}
 }
