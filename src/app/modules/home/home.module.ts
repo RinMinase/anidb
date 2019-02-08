467b86b47cdb6216f3c5575b4577790a957c2061
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-09 00:04:29 +0800

    TRANSITION: added created components to home module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,13 +5,23 @@ import { ReactiveFormsModule } from "@angular/forms";
 
 import { ManageHomeComponent } from "./manage-home/manage-home.component";
 import { LegendModule } from "../../core/components/legend/legend.module";
+import { ViewHomeComponent } from "./view-home/view-home.component";
+import { AddHomeComponent } from "./add-home/add-home.component";
+import { UpdateHomeComponent } from "./update-home/update-home.component";
+import { HomeComponent } from "./home.component";
 
 const routes: Routes = [{
 	path: "", component: ManageHomeComponent,
 }];
 
 @NgModule({
-	declarations: [ManageHomeComponent],
+	declarations: [
+		HomeComponent,
+		ManageHomeComponent,
+		ViewHomeComponent,
+		AddHomeComponent,
+		UpdateHomeComponent,
+	],
 	imports: [
 		CommonModule,
 		RouterModule.forChild(routes),
