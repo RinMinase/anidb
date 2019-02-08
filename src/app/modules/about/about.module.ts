472864d6f531250051df3199231e3298116987a4
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-09 00:42:19 +0800

    TRANSITION: optimized imports on about module

--- src/app/modules/about/about.module.ts
+++ src/app/modules/about/about.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgbPaginationModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AboutComponent } from "./about.component";
 
@@ -12,7 +12,7 @@ const routes: Routes = [{ path: "", component: AboutComponent }];
 	imports: [
 		CommonModule,
 		RouterModule.forChild(routes),
-		NgbModule,
+		NgbPaginationModule,
 	],
 })
 export class AboutModule { }
