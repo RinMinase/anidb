be8985881e936d71c2e33ff2883e99d2bdd071bf
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-08 23:39:49 +0800

    TRANSITION: fixed byname module routing

--- src/app/modules/byname/byname.module.ts
+++ src/app/modules/byname/byname.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+
 import { BynameComponent } from "./byname.component";
+
+const routes: Routes = [{ path: "", component: BynameComponent }];
 
 @NgModule({
 	declarations: [BynameComponent],
 	imports: [
 		CommonModule,
+		RouterModule.forChild(routes),
 	],
 })
 export class BynameModule { }
