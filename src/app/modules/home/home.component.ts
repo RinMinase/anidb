9b1ddb834105ec6601002002a180829084d8cd44
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-09 00:04:45 +0800

    TRANSITION: added home component

---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "app-home",
+	template: "<router-outlet></router-outlet>",
+})
+export class HomeComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
