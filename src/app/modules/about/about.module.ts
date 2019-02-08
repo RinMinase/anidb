e915cc16c58325c8da45a6ef68a90273acd1779d
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-08 23:40:28 +0800

    TRANSITION: organized import statements

--- src/app/modules/about/about.module.ts
+++ src/app/modules/about/about.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { AboutComponent } from "./about.component";
 import { RouterModule, Routes } from "@angular/router";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
+import { AboutComponent } from "./about.component";
 
 const routes: Routes = [{ path: "", component: AboutComponent }];
 
