8873d92e5618ac82404ce76160005040f3475721
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-09 00:04:12 +0800

    TRANSITION: added view component in home module

---
+++ src/app/modules/home/view-home/view-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "app-view-home",
+	templateUrl: "./view-home.component.html",
+	styleUrls: ["./view-home.component.scss"],
+})
+export class ViewHomeComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
