48ee4a39d0a316dffda653cc7df7ba43cc74d942
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-13 00:24:05 +0800

    TRANSITION: renamed firebase query service to builder

--- src/app/core/builders/firebase-query.service.ts
+++ src/app/core/builders/firebase-query.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@angular/core";
 @Injectable({
 	providedIn: "root",
 })
-export class FirebaseQueryService {
+export class FirebaseQueryBuilder {
 	firebaseOptions = {
 		db: "anime",
 		id: null,
