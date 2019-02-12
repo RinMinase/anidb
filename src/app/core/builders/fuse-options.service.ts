c18428fda151a3f93ea50ac063d7c84005d949c2
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-13 00:24:24 +0800

    TRANSITION: renamed fuse service to fuse options builder

--- src/app/core/builders/fuse-options.service.ts
+++ src/app/core/builders/fuse-options.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@angular/core";
 @Injectable({
 	providedIn: "root",
 })
-export class FuseService {
+export class FuseOptionsBuilder {
 	fuseOptions = {
 		shouldSort: true,
 		threshold: 0.6,
