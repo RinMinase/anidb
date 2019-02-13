30f55e00cce6925f0e2ab9eb9470d3e204dc1631
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-13 23:14:51 +0800

    TRANSITION: replaced old service with new one

--- src/app/core/services/firebase.service.ts
+++ src/app/core/services/firebase.service.ts
@@ -5,6 +5,8 @@ import "firebase/auth";
 import "firebase/database";
 import "firebase/storage";
 
+import { FirebaseQuery } from "@builders/firebase-query.service";
+
 @Injectable({
 	providedIn: "root",
 })
@@ -30,34 +32,36 @@ export class FirebaseService {
 		});
 	}
 
-	retrieve(
-		db = "anime",
-		id = "",
-		limit = 0,
-		orderKey = "",
-		orderDesc = false,
-		inhdd = true,
-	) {
+	retrieve(params?: FirebaseQuery) {
+		const query = params || new FirebaseQuery;
+		const idQuery = (query.id) ? `/${query.id}` : "";
 
-		id = (id) ? `/${id}` : "";
-		limit = (!isNaN(limit)) ? limit : 0;
-
-		let orderKeyValid = false;
-		const inHddValue = (inhdd && db === "anime") ? 1 : 0;
-
-		if (orderKey || orderKey === "dateFinished") {
-			orderKeyValid = true;
-		}
-
-		firebase.database().ref(`/${db}${id}`).on("value", () => {});
-
-		if (id === "" && !limit && !orderKey && inHddValue) {
-			return firebase.database()
-				.ref(`/${db}${id}`)
-				.orderByChild("inhdd")
-				.equalTo(1)
-				.once("value")
-				.then((data) => this._objectToArray(data.val()));
+		if (!query.limit && !query.orderKey && query.inhdd) {
+			return new Promise((resolve) => {
+				firebase.database()
+					.ref(`/${query.db}${idQuery}`)
+					.orderByChild("inhdd")
+					.equalTo(1)
+					.on("value", (data) => resolve(this._objectToArray(data.val())));
+			});
+		} else if (query.limit && query.orderKey) {
+			if (query.orderDirection === "asc") {
+				return new Promise((resolve) => {
+					firebase.database()
+						.ref(`/${query.db}${idQuery}`)
+						.orderByChild(query.orderKey)
+						.limitToFirst(query.limit)
+						.on("value", (data) => resolve(this._objectToArray(data.val())));
+				});
+			} else {
+				return new Promise((resolve) => {
+					firebase.database()
+						.ref(`/${query.db}${idQuery}`)
+						.orderByChild(query.orderKey)
+						.limitToLast(query.limit)
+						.on("value", (data) => resolve(this._objectToArray(data.val())));
+				});
+			}
 		}
 
 		return Promise.reject();
