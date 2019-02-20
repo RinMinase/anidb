575589dc448b998af2b5325814cf92277b1068b7
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-20 22:40:04 +0800

    TRANSITION: merged test scripts of hdd module for simpler file structure

--- src/app/modules/hdd/hdd.component.spec.ts
+++ src/app/modules/hdd/hdd.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { HddComponent } from "./hdd.component";
+import { ManageHddComponent } from "./manage-hdd/manage-hdd.component";
 
 describe("HddComponent", () => {
 	// let component: HddComponent;
@@ -15,6 +16,28 @@ describe("HddComponent", () => {
 
 	// beforeEach(() => {
 	// 	fixture = TestBed.createComponent(HddComponent);
+	// 	component = fixture.componentInstance;
+	// 	fixture.detectChanges();
+	// });
+
+	// it("should create", () => {
+	// 	expect(component).toBeTruthy();
+	// });
+});
+
+describe("ManageHddComponent", () => {
+	// let component: ManageHddComponent;
+	// let fixture: ComponentFixture<ManageHddComponent>;
+
+	// beforeEach(async(() => {
+	// 	TestBed.configureTestingModule({
+	// 		declarations: [ ManageHddComponent ],
+	// 	})
+	// 	.compileComponents();
+	// }));
+
+	// beforeEach(() => {
+	// 	fixture = TestBed.createComponent(ManageHddComponent);
 	// 	component = fixture.componentInstance;
 	// 	fixture.detectChanges();
 	// });
