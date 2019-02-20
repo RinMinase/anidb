e6014f12f4580c4796236f192ba686335740e731
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-20 22:40:27 +0800

    TRANSITION: merged test scripts of summer module for simpler file structure

--- src/app/modules/summer/summer.component.spec.ts
+++ src/app/modules/summer/summer.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { SummerComponent } from "./summer.component";
+import { ManageSummerComponent } from "./manage-summer/manage-summer.component";
 
 describe("SummerComponent", () => {
 	// let component: SummerComponent;
@@ -15,6 +16,28 @@ describe("SummerComponent", () => {
 
 	// beforeEach(() => {
 	// 	fixture = TestBed.createComponent(SummerComponent);
+	// 	component = fixture.componentInstance;
+	// 	fixture.detectChanges();
+	// });
+
+	// it("should create", () => {
+	// 	expect(component).toBeTruthy();
+	// });
+});
+
+describe("ManageSummerComponent", () => {
+	// let component: ManageSummerComponent;
+	// let fixture: ComponentFixture<ManageSummerComponent>;
+
+	// beforeEach(async(() => {
+	// 	TestBed.configureTestingModule({
+	// 		declarations: [ ManageSummerComponent ],
+	// 	})
+	// 	.compileComponents();
+	// }));
+
+	// beforeEach(() => {
+	// 	fixture = TestBed.createComponent(ManageSummerComponent);
 	// 	component = fixture.componentInstance;
 	// 	fixture.detectChanges();
 	// });
