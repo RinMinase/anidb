bd11b07b0237850c10295500bb7427f716483424
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-23 01:46:13 +0800

    TRANSITION: functional excel component in export module

--- src/app/modules/export/export-excel/export-excel.component.ts
+++ src/app/modules/export/export-excel/export-excel.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from "@angular/core";
 
+import * as xlsx from "xlsx";
+
 @Component({
 	selector: "app-excel",
 	templateUrl: "./export-excel.component.html",
-	styleUrls: ["./export-excel.component.scss"],
 })
 export class ExportExcelComponent implements OnInit {
 
 	constructor() { }
 
 	ngOnInit() {
+	}
+
+	exportExcel() {
+		const data = [
+			{ "Column 1": "Data 1", "Column 2": 44 },
+			{ "Column 1": "Data 2", "Column 3": "Another Data" },
+		];
+
+		const sheet = xlsx.utils.json_to_sheet(data);
+		const workbook = xlsx.utils.book_new();
+
+		xlsx.utils.book_append_sheet(workbook, sheet, "Data");
+		xlsx.writeFile(workbook, "sheet.xlsx");
 	}
 
 }
