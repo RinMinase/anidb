9b529b6f0578f99aba036bc76925ed9ffff403e6
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-23 01:50:17 +0800

    TRANSITION: removed unused stylesheet

--- src/app/modules/hdd/manage-hdd/manage-hdd.component.ts
+++ src/app/modules/hdd/manage-hdd/manage-hdd.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from "@angular/core";
 @Component({
 	selector: "app-manage-hdd",
 	templateUrl: "./manage-hdd.component.html",
-	styleUrls: ["./manage-hdd.component.scss"],
 })
 export class ManageHddComponent implements OnInit {
 
