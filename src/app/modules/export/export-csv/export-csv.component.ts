765b3b511f5afd68e9b33040ba0968cf943fef68
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-23 01:45:04 +0800

    TRANSITION: functional csv component in export module

--- src/app/modules/export/export-csv/export-csv.component.ts
+++ src/app/modules/export/export-csv/export-csv.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from "@angular/core";
 
+import * as xlsx from "xlsx";
+
 @Component({
 	selector: "app-csv",
 	templateUrl: "./export-csv.component.html",
-	styleUrls: ["./export-csv.component.scss"],
 })
 export class ExportCsvComponent implements OnInit {
 
 	constructor() { }
 
 	ngOnInit() {
+	}
+
+	exportCSV() {
+		const data = [
+			{ "Column 1": "Data 1", "Column 2": 44 },
+			{ "Column 1": "Data 2", "Column 3": "Another Data" },
+		];
+
+		const sheet = xlsx.utils.json_to_sheet(data);
+		const workbook = xlsx.utils.book_new();
+
+		xlsx.utils.book_append_sheet(workbook, sheet, "Data");
+		xlsx.writeFile(workbook, "sheet.csv");
 	}
 
 }
