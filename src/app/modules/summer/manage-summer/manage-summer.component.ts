bcff13abc438961b6bae5e203e1178304f9d4777
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-23 02:28:57 +0800

    TRANSITION: removed unused stylesheet

--- src/app/modules/summer/manage-summer/manage-summer.component.ts
+++ src/app/modules/summer/manage-summer/manage-summer.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from "@angular/core";
 @Component({
 	selector: "app-manage-summer",
 	templateUrl: "./manage-summer.component.html",
-	styleUrls: ["./manage-summer.component.scss"],
 })
 export class ManageSummerComponent implements OnInit {
 
