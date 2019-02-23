36a32f6fe32bd36fa163517e1df439daa040ac4d
Rin Minase <rin.black.raison.detre@gmail.com>
2019-02-23 15:59:51 +0800

    TRANSITION: moved functions to changelog component

--- src/app/modules/about/about.component.ts
+++ src/app/modules/about/about.component.ts
@@ -21,32 +21,16 @@ export class AboutComponent implements OnInit {
 	totalFilesizeGB: string;
 	totalFilesizeTB: string;
 
-	totalDuration = {
-		days: null,
-		hours: null,
-		minutes: null,
-		seconds: null,
-	};
-
-	quality = {
-		uhd: 0,
-		fhd: 0,
-		hd: 0,
-		hq: 0,
-		lq: 0,
-	};
+	totalDuration = { days: null, hours: null, minutes: null, seconds: null };
+	quality = { uhd: 0, fhd: 0, hd: 0, hq: 0, lq: 0 };
 
 	chart = {
-		data: [{
-			data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			label: "Series A",
-		}],
+		data: [{ data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], label: "Series A" }],
 		colors: [],
 		labels: [],
 		options: {},
 	};
 
-	githubCommits = [];
 	githubIssues = [];
 	packageIssues: Object[];
 	transitionProgress = transitionProgress;
@@ -69,7 +53,6 @@ export class AboutComponent implements OnInit {
 		}).catch(() => this.router.navigateByUrl("/login"));
 
 		this.getFirebaseImages();
-		this.getGithubCommits();
 		this.getGithubIssues();
 		this.getPackageIssues();
 		this.generateChartData();
@@ -139,69 +122,6 @@ export class AboutComponent implements OnInit {
 
 		this.firebase.retrieveImageUrl("/assets/user.jpg")
 			.then((url) => { this.userImage = url as string; });
-	}
-
-	private getGithubCommits() {
-		this.github.getCommits()
-			.subscribe((response) => {
-				const formattedCommits = {};
-
-				response.body.map((data: any) => {
-					if (data.commit.message.indexOf("Merge branch") === -1) {
-						const { date } = data.commit.author;
-						let rawMessage = data.commit.message.split(":");
-
-						if (rawMessage.length === 1) {
-							rawMessage = data.commit.message.split(new RegExp(" (.+)"));
-						}
-
-						if (rawMessage[1].indexOf(", resolved #") !== -1) {
-							rawMessage[1] = rawMessage[1].replace(new RegExp(", resolved #[0-9]+", "ig"), "");
-						}
-
-						const rawModule = rawMessage[0]
-							.trimStart()
-							.toLowerCase()
-							.replace(new RegExp("_", "g"), " ");
-						const module = (rawModule === "anidb" || rawModule === "transition") ? "" : rawModule;
-						const message = rawMessage[1].trimStart();
-						const commitDate = `c${moment(new Date(date)).format("YYYYMMDD")}`;
-						const title = moment(new Date(date)).format("MMM DD, YYYY");
-						const commitData = {
-							date: this.convertDate(date),
-							email: data.commit.author.email,
-							name: data.commit.author.name,
-							message,
-							module,
-							url: data.html_url,
-						};
-
-						if (!formattedCommits[commitDate]) {
-							formattedCommits[commitDate] = {
-								fix: [],
-								new: [],
-								improve: [],
-								title,
-							};
-						}
-
-						if (message.indexOf("fixed") !== -1
-							|| message.indexOf("removed") !== -1) {
-								formattedCommits[commitDate].fix.push(commitData);
-						} else if (message.indexOf("added") !== -1
-							|| message.indexOf("functional") !== -1
-							|| message.indexOf("migrated") !== -1) {
-								formattedCommits[commitDate].new.push(commitData);
-						} else {
-							formattedCommits[commitDate].improve.push(commitData);
-						}
-					}
-				});
-
-				Object.keys(formattedCommits).map((index) => {
-					this.githubCommits.push(formattedCommits[index]);
-				});
-			});
 	}
 
 	private getGithubIssues() {
--- src/app/modules/about/changelog/changelog.component.ts
+++ src/app/modules/about/changelog/changelog.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
+import * as moment from "moment-mini";
+
+import { GithubService } from "@services/github.service";
 
 @Component({
 	selector: "app-changelog",
@@ -7,11 +10,80 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class ChangelogComponent implements OnInit {
 
-	@Input() githubCommits: Array<any>;
+	githubCommits = [];
 
-	constructor() { }
+	constructor(private github: GithubService) { }
 
 	ngOnInit() {
+		this.github.getCommits().subscribe((response) => {
+			const formattedCommits = {};
+
+			response.body.map((data: any) => {
+				if (data.commit.message.indexOf("Merge branch") === -1) {
+					const { date } = data.commit.author;
+					let rawMessage = data.commit.message.split(":");
+
+					if (rawMessage.length === 1) {
+						rawMessage = data.commit.message.split(new RegExp(" (.+)"));
+					}
+
+					if (rawMessage[1].indexOf(", resolved #") !== -1) {
+						rawMessage[1] = rawMessage[1].replace(new RegExp(", resolved #[0-9]+", "ig"), "");
+					}
+
+					const rawModule = rawMessage[0]
+						.trimStart()
+						.toLowerCase()
+						.replace(new RegExp("_", "g"), " ");
+					const module = (rawModule === "anidb" || rawModule === "transition") ? "" : rawModule;
+					const message = rawMessage[1].trimStart();
+					const commitDate = `c${moment(new Date(date)).format("YYYYMMDD")}`;
+					const title = moment(new Date(date)).format("MMM DD, YYYY");
+					const commitData = {
+						date: this.convertDate(date),
+						email: data.commit.author.email,
+						name: data.commit.author.name,
+						message,
+						module,
+						url: data.html_url,
+					};
+
+					if (!formattedCommits[commitDate]) {
+						formattedCommits[commitDate] = {
+							fix: [],
+							new: [],
+							improve: [],
+							title,
+						};
+					}
+
+					if (message.indexOf("fixed") !== -1 || message.indexOf("removed") !== -1) {
+						formattedCommits[commitDate].fix.push(commitData);
+					} else if (message.indexOf("added") !== -1
+						|| message.indexOf("functional") !== -1
+						|| message.indexOf("migrated") !== -1) {
+
+						formattedCommits[commitDate].new.push(commitData);
+					} else {
+						formattedCommits[commitDate].improve.push(commitData);
+					}
+				}
+			});
+
+			Object.keys(formattedCommits).map((index) => {
+				this.githubCommits.push(formattedCommits[index]);
+			});
+		});
+	}
+
+	private convertDate(date: string, omitSeconds = false) {
+		if (!omitSeconds) {
+			return moment(new Date(date))
+				.format("MMM DD, YYYY HH:mm:ss");
+		} else {
+			return moment(new Date(date))
+				.format("MMM DD, YYYY HH:mm");
+		}
 	}
 
 }
