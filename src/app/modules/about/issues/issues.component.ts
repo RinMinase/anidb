5013efc632ee73d2a27bd9bb027756fb2676db5c
Rin Minase <rin.black.raison.detre@gmail.com>
2019-03-12 00:22:21 +0800

    ABOUT: updated package issues list

--- src/app/modules/about/issues/issues.component.ts
+++ src/app/modules/about/issues/issues.component.ts
@@ -68,6 +68,11 @@ export class IssuesComponent implements OnInit {
 			severity: "red",
 			version: "0.0.0",
 		}, {
+			package: "ng2-charts",
+			reason: "Still in beta stages",
+			severity: "green",
+			version: "2.0.0-beta.21",
+		}, {
 			package: "typescript",
 			reason: "Angular 7 only supports typescript versions below 3.3",
 			severity: "orange",
