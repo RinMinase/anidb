3caee0b38962adf0521e16af1fea527419354a5a
Rin Minase <rin.black.raison.detre@gmail.com>
2019-04-02 01:15:55 +0800

    ANIDB: functional code coverage in codecov

--- src/assets/config/testing/karma.conf.ts
+++ src/assets/config/testing/karma.conf.ts
@@ -1,22 +1,21 @@
 module.exports = (config) => {
 	config.set({
-		basePath: "",
+		basePath: "../../../../",
 		frameworks: ["jasmine", "@angular-devkit/build-angular"],
 		plugins: [
 			require("karma-jasmine"),
 			require("karma-chrome-launcher"),
-			require("karma-jasmine-html-reporter"),
-			require("karma-coverage-istanbul-reporter"),
 			require("@angular-devkit/build-angular/plugins/karma"),
 			require("karma-coverage"),
+			require("karma-coverage-istanbul-reporter"),
 		],
 		client: { clearContext: false },
 		coverageIstanbulReporter: {
-			dir: require("path").join(__dirname, "../../../../coverage"),
-			reports: ["html", "lcovonly", "text-summary"],
+			dir: "coverage",
+			reports: ["lcov", "text-summary"],
 			fixWebpackSourcePaths: true,
 		},
-		reporters: ["progress", "kjhtml", "coverage"],
+		reporters: ["progress", "coverage"],
 		port: 9876,
 		colors: true,
 		logLevel: config.LOG_WARN,
