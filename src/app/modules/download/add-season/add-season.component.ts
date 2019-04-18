e9685c2480fcb4cb8be47b90a926c6d529bcebf7
Rin Minase <rin.black.raison.detre@gmail.com>
2019-04-19 03:50:24 +0800

    DOWNLOAD: added season component

---
+++ src/app/modules/download/add-season/add-season.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import Swal from "sweetalert2";
+
+@Component({
+	selector: "app-add-season",
+	templateUrl: "./add-season.component.html",
+	styleUrls: ["./add-season.component.scss"],
+})
+export class AddSeasonComponent implements OnInit {
+
+	constructor(private modal: NgbActiveModal) { }
+
+	ngOnInit() {
+	}
+
+	cancel() {
+		Swal.fire({
+			title: "Are you sure?",
+			text: "This will discard your edits",
+			type: "question",
+			showCancelButton: true,
+		}).then((result) => {
+			if (result.value) { this.modal.dismiss(); }
+		});
+	}
+
+}
