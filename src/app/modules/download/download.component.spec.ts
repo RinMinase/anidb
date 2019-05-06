bac47f4d6b9acf2d4bf36f4922ee56928d190e35
Rin Minase <rin.black.raison.detre@gmail.com>
2019-05-07 01:31:37 +0800

    DOWNLOAD: fixed build errors

--- src/app/modules/download/download.component.spec.ts
+++ src/app/modules/download/download.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+// import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 // import { DownloadComponent } from "./download.component";
 // import { AddTitleComponent } from "./add-title/add-title.component";
