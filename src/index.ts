f1d1ab0b730173ecbf9bccaf8c65678843486d58
Rin Minase <rin.black.raison.detre@gmail.com>
2019-11-10 06:14:51 +0800

    ANIDB: moved main module

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "zone.js/dist/zone";
 
 import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import { AppModule } from "./app/app.module";
+import { AppModule } from "./app.module";
 
 const { error } = console;
 
