d8adad238bfe31556286cead15460675bef0066e
Rin Minase <rin.black.raison.detre@gmail.com>
2020-01-21 14:40:26 +0800

    ANIDB: fixed global localized import error in bootstrap

--- assets/config/testing/test.ts
+++ assets/config/testing/test.ts
@@ -1,5 +1,6 @@
 import "zone.js/dist/zone";
 import "zone.js/dist/zone-testing";
+import "@angular/localize/init";
 import { getTestBed } from "@angular/core/testing";
 import {
 	BrowserDynamicTestingModule,
