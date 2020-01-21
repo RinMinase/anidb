ff34306e8d9c14edc53a56db9089c2b36b221c7f
Rin Minase <rin.black.raison.detre@gmail.com>
2020-01-22 01:01:33 +0800

    API: added service

--- src/core/services/_services.spec.ts
+++ src/core/services/_services.spec.ts
@@ -4,6 +4,7 @@ import { DarkModeService } from '@services/dark-mode.service';
 // import { FirebaseService } from "@services/firebase.service";
 // import { GithubService } from "@services/github.service";
 // import { UtilityService } from "@services/utility.service";
+// import { ApiService } from "@services/api.service";
 
 describe("DarkModeService", () => {
 	beforeEach(() => TestBed.configureTestingModule({}));
@@ -47,6 +48,20 @@ describe("UtilityService", () => {
 
 	// it("should be created", () => {
 	// 	const service: UtilityService = TestBed.get(UtilityService);
+	// 	expect(service).toBeTruthy();
+	// });
+});
+
+// ng9 generated test case
+describe("ApiService", () => {
+	// let service: ApiService;
+
+	// beforeEach(() => {
+	// 	TestBed.configureTestingModule({});
+	// 	service = TestBed.inject(ApiService);
+	// });
+
+	// it("should be created", () => {
 	// 	expect(service).toBeTruthy();
 	// });
 });
