94cd67bf7274b7ee8f1319a495e0cb0efc83af26
Rin Minase <rin.black.raison.detre@gmail.com>
2020-01-22 11:12:26 +0800

    ANIDB: added env dependency

--- assets/config/webpack.ts
+++ assets/config/webpack.ts
@@ -1,8 +1,10 @@
 // tslint:disable-next-line:no-var-requires
+const Dotenv = require("dotenv-webpack");
 const IgnorePlugin = require("webpack").IgnorePlugin;
 
 module.exports = {
 	plugins: [
+		new Dotenv(),
 		new IgnorePlugin(/^\.\/locale$/, /moment$/),
 	],
 	resolve: {
