2f3bd61493473419c1c50bf08035d78df5998a01
Rin Minase <rin.black.raison.detre@gmail.com>
2020-01-31 13:58:55 +0800

    ANIDB: removed usages of dotenv

--- assets/config/webpack.ts
+++ assets/config/webpack.ts
@@ -1,10 +1,8 @@
 // tslint:disable-next-line:no-var-requires
-const Dotenv = require("dotenv-webpack");
 const IgnorePlugin = require("webpack").IgnorePlugin;
 
 module.exports = {
 	plugins: [
-		new Dotenv(),
 		new IgnorePlugin(/^\.\/locale$/, /moment$/),
 	],
 	resolve: {
