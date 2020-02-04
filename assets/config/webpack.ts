d6396c8aced6d82e7e432cc84869710a73de3a35
Rin Minase <rin.black.raison.detre@gmail.com>
2020-02-04 22:53:01 +0800

    ANIDB: reappended env plugin in webpack config

--- assets/config/webpack.ts
+++ assets/config/webpack.ts
@@ -1,8 +1,14 @@
-// tslint:disable-next-line:no-var-requires
+/* tslint:disable:no-var-requires */
+const Dotenv = require('dotenv-webpack');
 const IgnorePlugin = require("webpack").IgnorePlugin;
+/* tslint:enable */
 
 module.exports = {
 	plugins: [
+		new Dotenv({
+			systemvars: true,
+			silent: true,
+		}),
 		new IgnorePlugin(/^\.\/locale$/, /moment$/),
 	],
 	resolve: {
