e7dac3228c820247a9c387db295dca16b75fc21a
Rin Minase <rin.black.raison.detre@gmail.com>
2020-05-28 15:35:10 +0800

    ABOUT: updated unit specs

--- src/about/unit-specs/unit-specs.component.ts
+++ src/about/unit-specs/unit-specs.component.ts
@@ -14,12 +14,14 @@ export class UnitSpecsComponent implements OnInit {
 		this.data = {
 			cpu: "Intel Core i7-9700K",
 			cpuSubtext: "(All Core OC) 5.0 GHz @ 1.34v",
+			cpuSubtext2: "x44 Cache Overclock",
 			ram: "GSkill Trident Z RGB 32GB",
 			ramSubtext: "(4 × 8GB) CL16 3000MHz DDR4",
+			ramSubtext2: "(OC) 3200MHz 16-18-38 1.38v",
 			gpu: "Asus ROG Strix GTX 1070 OC",
-			gpuSubtext: "(8GB DDR5)",
+			gpuSubtext: "(8GB DDR5) +30 Core, +1K Mem",
 
-			motherboard: "Asus ROG Strix Z390-E Gaming",
+			motherboard: "Asus ROG Strix Z390-E",
 			psu: "CoolerMaster MasterWatt 650W",
 			psuSubtext: "80+ Bronze",
 			cooler: "Noctua NH-U12S chromax",
@@ -30,7 +32,7 @@ export class UnitSpecsComponent implements OnInit {
 			case: "NZXT H700 Matte Black",
 			fans: "3x Noctua NF-S12A chromax",
 			fansSubtext: "3x Thermaltake Riing Plus 120",
-			fansSubtext2: "Thermaltake Riing Fan Controller",
+			fansSubtext2: "Thermaltake Fan Controller",
 
 			monitor: "Dell SE2717H (1920 × 1080) 75Hz",
 			speakers: "Bose Companion 50 2.1",
