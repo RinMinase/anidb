271972d5ee9fb159c3a50b598cc64c17f52f482e
Rin Minase <rin.black.raison.detre@gmail.com>
2021-12-24 12:11:09 +0800

    ABOUT: improved frameworks tooltips

--- src/about/frameworks/frameworks.component.ts
+++ src/about/frameworks/frameworks.component.ts
@@ -10,6 +10,54 @@ import { FirebaseService } from "@services/firebase.service";
 export class FrameworksComponent implements OnInit {
 	constructor(private firebase: FirebaseService) {}
 
+	frameworks	= [
+		{
+			cls: "angular",
+			label: "Angular 13",
+			url: "https://angular.io",
+		},
+		{
+			cls: "ts",
+			label: "Typescript 4.5",
+			url: "https://www.typescriptlang.org"
+		},
+		{
+			cls: "scss",
+			label: "SCSS",
+			url: "https://sass-lang.com"
+		},
+		{
+			cls: "bootstrap",
+			label: "Bootstrap 5",
+			url: "https://getbootstrap.com"
+		},
+		{
+			cls: "ngb",
+			label: "Angular-Bootstrap",
+			url: "https://ng-bootstrap.github.io"
+		},
+		{
+			cls: "firebase",
+			label: "Firebase",
+			url: "https://firebase.google.com"
+		},
+		{
+			cls: "webpack",
+			label: "Webpack",
+			url: "https://webpack.js.org"
+		},
+		{
+			cls: "cordova",
+			label: "Cordova",
+			url: "https://cordova.apache.org"
+		},
+		{
+			cls: "circle",
+			label: "Circle CI",
+			url: "https://circleci.com"
+		},
+	]
+
 	ngOnInit() {
 		this.firebase.retrieveImageUrl("/assets/ngx-frameworks.png").then((url) => {
 			this.element("a.frameworks").forEach((element) => {
