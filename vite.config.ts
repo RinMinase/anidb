ed0ef04f5e77df2a583f73aa120f2790653b4f01
Rin Minase <rin.black.raison.detre@gmail.com>
2022-07-27 13:45:06 +0800

    ✨ feat(pwa): appended PWA setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), VitePWA()],
   esbuild: {
     logOverride: { 'this-is-undefined-in-esm': 'silent' }
   },
