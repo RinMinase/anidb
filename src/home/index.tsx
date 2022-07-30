40565897554012f9f410193a78940eed2417bcca
Rin Minase <rin.black.raison.detre@gmail.com>
2022-07-31 03:58:07 +0800

    ✨ feat(home): added blank home page

---
+++ src/home/index.tsx
@@ -0,0 +1,9 @@
+import Nav from "@components/nav";
+
+const Home = () => {
+  return (
+    <div>home</div>
+  );
+};
+
+export default Home;
--- src/routes.tsx
+++ src/routes.tsx
@@ -4,15 +4,19 @@ import { h } from "preact";
 import { Router, Route } from "preact-router";
 import AsyncRoute from "preact-async-route";
 
-import Test from "./test";
+import Home from "./home";
 import Login from "./login";
+
 import Page404 from "@components/Page404";
+
+import Test from "./test";
 
 const Lazy = () => import("./lazy").then((c) => c.default);
 
 const Routes = () => (
   <Router>
     <Route path="/" component={Login} />
+    <Route path="/home" component={Home} />
 
     <Route path="/test" component={Test} />
     <AsyncRoute path="/lazy" getComponent={Lazy} />
