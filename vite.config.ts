d6388491b429c794729ae02343393fafa190c4fb
Rin Minase <rin.black.raison.detre@gmail.com>
2022-07-31 00:12:10 +0800

    ✨ feat: added common components path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+
 import { VitePWA } from 'vite-plugin-pwa'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), VitePWA()],
+  plugins: [preact(), tsconfigPaths(), VitePWA()],
   esbuild: {
     logOverride: { 'this-is-undefined-in-esm': 'silent' }
   },
