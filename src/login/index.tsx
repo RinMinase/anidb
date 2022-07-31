c6e4b75cba84934fb9a63862d0d1fa7f0485aadc
Rin Minase <rin.black.raison.detre@gmail.com>
2022-08-01 01:51:26 +0800

    💎 style(login): removed unused code

--- src/login/index.tsx
+++ src/login/index.tsx
@@ -1,28 +1,15 @@
-import { useContext } from "preact/hooks";
 import { useForm } from "react-hook-form";
 
 import {
-  Box,
   Button,
   Grid,
-  IconButton,
   Stack,
   styled,
   TextField,
   Typography,
-  useTheme,
 } from "@mui/material";
 
-import { Brightness4, Brightness7 } from "@mui/icons-material";
-
-import { ColorModeContext } from "../main";
 import { Form, resolver } from "./validation";
-
-const DarkModeContainer = styled(Box)({
-  position: "absolute",
-  right: 0,
-  top: 0,
-});
 
 const LoginContainer = styled(Grid)({
   height: "calc(100vh - 48px)",
@@ -35,9 +22,6 @@ const LoginStack = styled(Stack)({
 });
 
 const Login = () => {
-  const theme = useTheme();
-  const colorMode = useContext(ColorModeContext);
-
   const {
     register,
     handleSubmit,
@@ -49,43 +33,35 @@ const Login = () => {
   };
 
   return (
-    <>
-      <LoginContainer container justifyContent="center">
-        <Grid item xs={12} sm={6} md={4}>
-          <form onSubmit={handleSubmit(handleSubmitForm)}>
-            <LoginStack spacing={3}>
-              <Typography variant="h4">Login</Typography>
+    <LoginContainer container justifyContent="center">
+      <Grid item xs={12} sm={6} md={4}>
+        <form onSubmit={handleSubmit(handleSubmitForm)}>
+          <LoginStack spacing={3}>
+            <Typography variant="h4">Login</Typography>
 
-              <TextField
-                variant="outlined"
-                label="Email Address"
-                error={!!errors.email}
-                helperText={errors.email?.message}
-                {...register("email")}
-              />
-              <TextField
-                type="password"
-                variant="outlined"
-                label="Password"
-                error={!!errors.password}
-                helperText={errors.password?.message}
-                {...register("password")}
-              />
+            <TextField
+              variant="outlined"
+              label="Email Address"
+              error={!!errors.email}
+              helperText={errors.email?.message}
+              {...register("email")}
+            />
+            <TextField
+              type="password"
+              variant="outlined"
+              label="Password"
+              error={!!errors.password}
+              helperText={errors.password?.message}
+              {...register("password")}
+            />
 
-              <Button variant="contained" size="large" type="submit">
-                Login
-              </Button>
-            </LoginStack>
-          </form>
-        </Grid>
-      </LoginContainer>
-
-      {/* <DarkModeContainer>
-        <IconButton onClick={colorMode.toggleColorMode} color="inherit">
-          {theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness4 />}
-        </IconButton>
-      </DarkModeContainer> */}
-    </>
+            <Button variant="contained" size="large" type="submit">
+              Login
+            </Button>
+          </LoginStack>
+        </form>
+      </Grid>
+    </LoginContainer>
   );
 };
 
