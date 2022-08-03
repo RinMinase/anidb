970c19a53d914f01ac8354613af651305f0767f6
Rin Minase <rin.black.raison.detre@gmail.com>
2022-08-03 18:39:54 +0800

    ♻️ refactor(home): lazy loaded home page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,19 +2,19 @@ import { h } from "preact";
 import { Router, Route } from "preact-router";
 import AsyncRoute from "preact-async-route";
 
-import Home from "./home";
 import Login from "./login";
 
 import Page404 from "@components/pages/Page404";
 
 const Registration = () => import("./registration").then((c) => c.default);
+const Home = () => import("./home").then((c) => c.default);
 
 const Routes = () => (
   <Router>
     <Route path="/" component={Login} />
-    <Route path="/home" component={Home} />
 
     <AsyncRoute path="/register" getComponent={Registration} />
+    <AsyncRoute path="/home" getComponent={Home} />
 
     <Route default component={Page404} />
   </Router>
