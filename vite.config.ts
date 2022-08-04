0cffc353639fdd699779e9fbf19710506d00c29e
Rin Minase <rin.black.raison.detre@gmail.com>
2022-08-05 03:24:48 +0800

    ✨ feat: added preconnection to api url

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
 import { defineConfig } from "vite";
+import dotenv from "dotenv";
 
 import preact from "@preact/preset-vite";
 
 import tsconfigPaths from "vite-tsconfig-paths";
 import viteCompression from "vite-plugin-compression";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 import { VitePWA } from "vite-plugin-pwa";
 
+const { parsed } = dotenv.config();
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), tsconfigPaths(), VitePWA(), viteCompression()],
+  plugins: [
+    preact(),
+    createHtmlPlugin({ inject: { data: parsed } }),
+    tsconfigPaths(),
+    VitePWA(),
+    viteCompression(),
+  ],
   esbuild: {
     logOverride: { "this-is-undefined-in-esm": "silent" },
   },
