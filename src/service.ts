0d5f9809be7db8f528a2b6e5047bee4f29831a36
Rin Minase <rin.black.raison.detre@gmail.com>
2022-08-07 17:01:13 +0800

    ✨ feat: added rerouting on unauthorized responses

--- src/service.ts
+++ src/service.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
+import { route } from "preact-router";
 
 const removeTrailSlashes = (str: string): string => {
   let i = str.length;
 
-  while (str[--i] === '/');
+  while (str[--i] === "/");
 
-  return str.slice(0, i+1);
-}
+  return str.slice(0, i + 1);
+};
 
 let API_URL = import.meta.env.VITE_API_URL || "";
 
@@ -17,10 +18,25 @@ if (API_URL) {
   axios.defaults.headers.post["Content-Type"] = "application/json";
   axios.defaults.withCredentials = true;
 
+  axios.interceptors.response.use(
+    (res) => res,
+    (err) => {
+      if (
+        err.response.status === 401 &&
+        err.response.message == "Unauthorized"
+      ) {
+        localStorage.clear();
+        route("/");
+      }
+
+      return Promise.reject(err);
+    },
+  );
+
   if (localStorage.getItem("authToken")) {
     axios.defaults.headers.common = {
+      ...axios.defaults.headers.common,
       Authorization: `Bearer ${localStorage.getItem("authToken")}`,
     };
   }
 }
-
