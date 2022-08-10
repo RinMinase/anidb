8c125748dc7e7397e6b2d23618fe7d34a5431121
Rin Minase <rin.black.raison.detre@gmail.com>
2022-08-11 01:02:11 +0800

    ✨ feat: improved dashboard tile component

--- src/common/DashboardTile.tsx
+++ src/common/DashboardTile.tsx
@@ -1,25 +1,34 @@
 import { Fragment } from "preact";
-import { Box, Divider, Paper, styled, Typography } from "@mui/material";
+import { Box, Divider, Grid, Paper, styled, Typography } from "@mui/material";
 
 type Props = {
+  onClick?: any;
   icon?: any;
   iconColor?: string;
   heading: string;
+  subHeading?: string;
   value: string | number | null | undefined;
   footer?: string;
   footers?: string[];
+  footerLeft?: string;
+  footerRight?: string;
+};
+
+type DashboardItemProps = {
+  hasOnclick?: boolean;
 };
 
 type DashboardIconProps = {
   iconColor?: string;
 };
 
-const DashboardItem = styled(Paper)({
+const DashboardItem = styled(Paper)<DashboardItemProps>(({ hasOnclick }) => ({
+  cursor: hasOnclick ? "pointer" : "",
   marginTop: 24,
   textAlign: "right",
   position: "relative",
   minHeight: 145,
-});
+}));
 
 const DashboardContainer = styled(Box)({
   padding: 16,
@@ -44,9 +53,13 @@ const DashboardFooter = styled(Box)(({ theme }) => ({
   padding: theme.spacing(1, 2),
 }));
 
+const FooterRight = styled(Grid)({
+  textAlign: "right",
+});
+
 const DashboardTile = (props: Props) => {
   return (
-    <DashboardItem>
+    <DashboardItem onClick={props.onClick} hasOnclick={!!props.onClick}>
       <DashboardContainer>
         {props.icon && (
           <DashboardIcon
@@ -57,7 +70,22 @@ const DashboardTile = (props: Props) => {
 
         <Typography variant="body2">{props.heading}</Typography>
         <Typography variant="h4">{props.value}</Typography>
+        {props.subHeading && (
+          <Typography variant="caption">{props.subHeading}</Typography>
+        )}
       </DashboardContainer>
+      {(props.footerLeft || props.footerRight) && (
+        <>
+          <Divider />
+          <DashboardFooter>
+            <Grid container justifyContent="space-between">
+              <Grid item>{props.footerLeft}</Grid>
+              <FooterRight item>{props.footerRight}</FooterRight>
+            </Grid>
+            <Typography variant="caption">{props.footer}</Typography>
+          </DashboardFooter>
+        </>
+      )}
       {props.footer && (
         <>
           <Divider />
