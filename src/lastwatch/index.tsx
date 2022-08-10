66c48a100c86b8d48dcebec434e5699fb9254eb4
Rin Minase <rin.black.raison.detre@gmail.com>
2022-08-10 10:47:11 +0800

    ✨ feat(lastwatch): modified to use dashboard tiles component

--- src/lastwatch/index.tsx
+++ src/lastwatch/index.tsx
@@ -1,6 +1,8 @@
+import { useContext, useEffect, useState } from "preact/hooks";
+import axios from "axios";
+
 import {
   Box,
-  Divider,
   Grid,
   Paper,
   styled,
@@ -10,21 +12,15 @@ import {
   TableContainer,
   TableHead,
   TableRow,
-  Typography,
 } from "@mui/material";
 
 import NumbersIcon from "@mui/icons-material/Numbers";
 import MovieIcon from "@mui/icons-material/MovieOutlined";
 import ScheduleIcon from "@mui/icons-material/Schedule";
 import SubscriptionsIcon from "@mui/icons-material/SubscriptionsOutlined";
-import { useContext, useEffect, useState } from "preact/hooks";
-import { GlobalLoaderContext, TableLoader } from "@components";
-import axios from "axios";
-import { Data, Stats } from "./types";
 
-type DashboardIconProps = {
-  iconColor?: string;
-};
+import { DashboardTile, GlobalLoaderContext, TableLoader } from "@components";
+import { Data, Stats } from "./types";
 
 const HomeContainer = styled(Box)({
   paddingTop: 24,
@@ -34,36 +30,6 @@ const HomeContainer = styled(Box)({
 const Dashboard = styled(Box)({
   marginBottom: 32,
 });
-
-const DashboardItem = styled(Paper)({
-  marginTop: 24,
-  textAlign: "right",
-  position: "relative",
-  minHeight: 145,
-});
-
-const DashboardContainer = styled(Box)({
-  padding: 16,
-});
-
-const DashboardIcon = styled(Box)<DashboardIconProps>(({ iconColor }) => ({
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  backgroundColor: iconColor || "#cecece",
-  color: "#fff",
-
-  position: "absolute",
-  top: -15,
-  left: 20,
-
-  width: 56,
-  height: 56,
-}));
-
-const DashboardFooter = styled(Box)(({ theme }) => ({
-  padding: theme.spacing(1, 2),
-}));
 
 const CustomTable = styled(Table)({
   minWidth: 650,
@@ -103,76 +69,40 @@ const LastWatch = () => {
       <Dashboard>
         <Grid container spacing={4}>
           <Grid item xs={12} sm={6} md={3}>
-            <DashboardItem>
-              <DashboardContainer>
-                <DashboardIcon
-                  iconColor="#ff9800"
-                  children={<NumbersIcon fontSize="large" />}
-                />
-                <Typography variant="body2">Total Episodes</Typography>
-                <Typography variant="h4">{stats.totalEps}</Typography>
-              </DashboardContainer>
-              <Divider />
-              <DashboardFooter>
-                <Typography variant="caption">
-                  Total Titles: {stats.totalTitles}
-                </Typography>
-              </DashboardFooter>
-            </DashboardItem>
+            <DashboardTile
+              icon={<NumbersIcon fontSize="large" />}
+              iconColor="#ff9800"
+              heading="Total Episodes"
+              value={stats.totalEps}
+              footer={`Total Titles: ${stats.totalTitles}`}
+            />
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
-            <DashboardItem>
-              <DashboardContainer>
-                <DashboardIcon
-                  iconColor="#2196f3"
-                  children={<ScheduleIcon fontSize="large" />}
-                />
-                <Typography variant="body2">Days since last entry</Typography>
-                <Typography variant="h4">{stats.daysLastEntry} days</Typography>
-              </DashboardContainer>
-              <Divider />
-              <DashboardFooter>
-                <Typography variant="caption">
-                  Days since oldest entry: {stats.daysOldestEntry}
-                </Typography>
-              </DashboardFooter>
-            </DashboardItem>
+            <DashboardTile
+              icon={<ScheduleIcon fontSize="large" />}
+              iconColor="#2196f3"
+              heading="Days since last entry"
+              value={stats.daysLastEntry}
+              footer={`Days since oldest entry: ${stats.daysOldestEntry}`}
+            />
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
-            <DashboardItem>
-              <DashboardContainer>
-                <DashboardIcon
-                  iconColor="#009688"
-                  children={<SubscriptionsIcon fontSize="large" />}
-                />
-                <Typography variant="body2">Titles per week</Typography>
-                <Typography variant="h4">{stats.titlesPerWeek}</Typography>
-              </DashboardContainer>
-              <Divider />
-              <DashboardFooter>
-                <Typography variant="caption">
-                  One-Cour (12 Eps) per week: {stats.coursPerWeek}
-                </Typography>
-              </DashboardFooter>
-            </DashboardItem>
+            <DashboardTile
+              icon={<SubscriptionsIcon fontSize="large" />}
+              iconColor="#009688"
+              heading="Titles per week"
+              value={stats.titlesPerWeek}
+              footer={`One-Cour (12 Eps) per week: ${stats.coursPerWeek}`}
+            />
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
-            <DashboardItem>
-              <DashboardContainer>
-                <DashboardIcon
-                  iconColor="#00bcd4"
-                  children={<MovieIcon fontSize="large" />}
-                />
-                <Typography variant="body2">Episodes per day</Typography>
-                <Typography variant="h4">{stats.epsPerDay}</Typography>
-              </DashboardContainer>
-              <Divider />
-              <DashboardFooter>
-                <Typography variant="caption">
-                  Episodes per week: {stats.epsPerWeek}
-                </Typography>
-              </DashboardFooter>
-            </DashboardItem>
+            <DashboardTile
+              icon={<MovieIcon fontSize="large" />}
+              iconColor="#00bcd4"
+              heading="Episodes per day"
+              value={stats.epsPerDay}
+              footer={`Episodes per week: ${stats.epsPerWeek}`}
+            />
           </Grid>
         </Grid>
       </Dashboard>
