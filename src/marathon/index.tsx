930ec1a8ec6bb6b99e57586030ad464e3be3170b
Rin Minase <rin.black.raison.detre@gmail.com>
2022-08-11 20:52:58 +0800

    ✨ feat(marathon): added marathon page table

--- src/marathon/index.tsx
+++ src/marathon/index.tsx
@@ -1,8 +1,133 @@
+import { useContext, useEffect, useState } from "preact/hooks";
+import axios from "axios";
+
+import {
+  Box,
+  Grid,
+  MenuItem,
+  MenuList,
+  Paper,
+  styled,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+
+import NumbersIcon from "@mui/icons-material/Numbers";
+import MovieIcon from "@mui/icons-material/MovieOutlined";
+import ScheduleIcon from "@mui/icons-material/Schedule";
+import SubscriptionsIcon from "@mui/icons-material/SubscriptionsOutlined";
+
+import { DashboardTile, GlobalLoaderContext, TableLoader } from "@components";
+import { Data, Sequences, Stats } from "./types";
+
+const ModuleContainer = styled(Box)({
+  paddingTop: 24,
+  paddingBottom: 24,
+});
+
+const Dashboard = styled(Box)({
+  marginBottom: 32,
+});
+
+const CustomTable = styled(Table)({
+  minWidth: 650,
+});
 
 const Marathon = () => {
+  const { isLoading, toggleLoader } = useContext(GlobalLoaderContext);
+  const [data, setData] = useState<Data>([]);
+  const [sequences, setSequences] = useState<Sequences>([]);
+  const [stats, setStats] = useState<Stats>({});
+
+  useEffect(() => {
+    toggleLoader(true);
+
+    axios
+      .get("/sequences")
+      .then(({ data: { data } }) => {
+        setSequences(() => data);
+
+        axios
+          .get("/entries/by-sequence/1")
+          .then(({ data: { data } }) => {
+            setData(() => data.data);
+            setStats(() => data.stats);
+          })
+          .catch((err) => console.error(err))
+          .finally(() => toggleLoader(false));
+      })
+      .catch((err) => {
+        console.error(err);
+        toggleLoader(false);
+      });
+  }, []);
+
+  const handleClickSequence = (id: number) => {
+    toggleLoader(true);
+
+    axios
+      .get(`/entries/by-sequence/${id}`)
+      .then(({ data: { data } }) => {
+        setData(() => data.data);
+        setStats(() => data.stats);
+      })
+      .catch((err) => console.error(err))
+      .finally(() => toggleLoader(false));
+  };
+
   return (
-    <div>marathon page</div>
-  )
-}
+    <ModuleContainer>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={5} md={3}>
+          <MenuList>
+            {sequences.map((item, index) => (
+              <MenuItem
+                key={`mara-${index}`}
+                onClick={() => handleClickSequence(item.id)}
+              >
+                {item.title}
+              </MenuItem>
+            ))}
+          </MenuList>
+        </Grid>
+        <Grid item xs={12} sm={7} md={9}>
+          <TableContainer component={Paper}>
+            <CustomTable>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Title</TableCell>
+                  <TableCell>E / O / S</TableCell>
+                  <TableCell>Filesize</TableCell>
+                  <TableCell>Date Finished</TableCell>
+                </TableRow>
+              </TableHead>
+
+              <TableBody>
+                {!isLoading ? (
+                  data.map((item) => (
+                    <TableRow hover key={item.id}>
+                      <TableCell>{item.title}</TableCell>
+                  <TableCell>
+                    {item.episodes} / {item.ovas} / {item.specials}
+                  </TableCell>
+                  <TableCell>{item.filesize}</TableCell>
+                  <TableCell>{item.dateFinished}</TableCell>
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableLoader />
+                )}
+              </TableBody>
+            </CustomTable>
+          </TableContainer>
+        </Grid>
+      </Grid>
+    </ModuleContainer>
+  );
+};
 
 export default Marathon;
---
+++ src/marathon/types.ts
@@ -0,0 +1,36 @@
+import { string } from "yup";
+
+export type Data = Array<{
+  id: string;
+  quality: string;
+  title: string;
+  filesize: string;
+  dateFinished?: string;
+  rewatched?: boolean;
+  episodes?: number;
+  ovas?: number;
+  specials?: number;
+}>;
+
+export type Sequences = Array<{
+  id: number;
+  title: string;
+  date_from: string;
+  date_to: string;
+}>;
+
+export type Stats = {
+  titles_per_day?: number;
+  eps_per_day?: number;
+  quality_2160?: number;
+  quality_1080?: number;
+  quality_720?: number;
+  quality_480?: number;
+  quality_360?: number;
+  total_titles?: number;
+  total_eps?: number;
+  total_size?: string;
+  total_days?: number;
+  start_date?: string;
+  end_date?: string;
+};
