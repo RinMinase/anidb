bb4632028ee753219edae77f12fa61d7acea8613
Rin Minase <rin.black.raison.detre@gmail.com>
2022-08-20 20:43:45 +0800

    ✨ feat(management): added data section

--- src/data-management/components/DataSection.tsx
+++ src/data-management/components/DataSection.tsx
@@ -1,8 +1,38 @@
+import { DashboardTile } from "@components";
+import { Grid } from "@mui/material";
 
-const DataSection = () => {
+import { Data } from "../types";
+
+type Props = {
+  data: Data;
+};
+
+const DataSection = (props: Props) => {
   return (
-    <div>data section</div>
-  )
-}
+    <Grid container spacing={4}>
+      <Grid item xs={12} sm={6} md={3}>
+        <DashboardTile
+          heading="Total Entries"
+          value={props.data.entries}
+          largeText
+        />
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <DashboardTile
+          heading="Total Buckets"
+          value={props.data.buckets}
+          largeText
+        />
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <DashboardTile
+          heading="Total Partials"
+          value={props.data.partials}
+          largeText
+        />
+      </Grid>
+    </Grid>
+  );
+};
 
 export default DataSection;
