c54d5959c1e1bb7c42d6bb40383d1959b40a16d3
Rin Minase <rin.black.raison.detre@gmail.com>
2022-08-20 20:44:09 +0800

    ✨ feat(management): added stats section

--- src/data-management/components/StatsSection.tsx
+++ src/data-management/components/StatsSection.tsx
@@ -1,8 +1,69 @@
+import { DashboardTile } from "@components";
+import { Grid } from "@mui/material";
 
-const StatsSection = () => {
+import { Stats } from "../types";
+
+type Props = {
+  stats: Stats;
+};
+
+const StatsSection = (props: Props) => {
   return (
-    <div>stats section</div>
-  )
-}
+    <>
+      <Grid container spacing={4}>
+        <Grid item xs={12} sm={6} md={3}>
+          <DashboardTile
+            heading="Total watch time"
+            value={props.stats.watch}
+            subHeading={props.stats.watchSubtext}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <DashboardTile
+            heading="Total rewatch time"
+            value={props.stats.rewatch}
+            subHeading={props.stats.rewatchSubtext}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <DashboardTile
+            heading="Total bucket size"
+            value={props.stats.bucketSize}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <DashboardTile
+            heading="Total entry size"
+            value={props.stats.entrySize}
+          />
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={4}>
+        <Grid item xs={12} sm={6} md={3}>
+          <DashboardTile
+            heading="Total episode count"
+            value={props.stats.episodes}
+            largeText
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <DashboardTile
+            heading="Total title count"
+            value={props.stats.titles}
+            largeText
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <DashboardTile
+            heading="Total season count"
+            value={props.stats.seasons}
+            largeText
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 export default StatsSection;
