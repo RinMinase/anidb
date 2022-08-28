0ddc3c47988d584a6d1226715c26d48b8e75ba46
Rin Minase <rin.black.raison.detre@gmail.com>
2022-08-28 18:41:43 +0800

    ♻️ refactor(sim): reduced code redundancy

---
+++ src/bucket-sim/_components.ts
@@ -0,0 +1,110 @@
+import { Box, Button, IconButton, OutlinedInput, Paper, styled, TableCell, TextField, Typography } from "@mui/material";
+
+const ModuleContainer = styled(Box)({
+  paddingTop: 24,
+  paddingBottom: 24,
+});
+
+const Header = styled(Box)({
+  display: "flex",
+  marginBottom: 32,
+});
+
+const ControlButtonsContainer = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column-reverse",
+    alignItems: "unset",
+  },
+}));
+
+const ControlButtions = styled(Button)(({ theme }) => ({
+  minWidth: 120,
+  marginLeft: 16,
+  [theme.breakpoints.down("sm")]: {
+    marginTop: 8,
+  },
+}));
+
+const DescriptionContainer = styled(Paper)({
+  marginBottom: 16,
+});
+
+const CustomIconButton = styled(IconButton)({
+  width: 32,
+  height: 32,
+});
+
+const CustomCell = styled(TableCell)(({ theme }) => ({
+  verticalAlign: "top",
+
+  [theme.breakpoints.down("sm")]: {
+    padding: 4,
+  },
+}));
+
+const CustomCellButton = styled(TableCell)(({ theme }) => ({
+  verticalAlign: "center",
+
+  [theme.breakpoints.down("sm")]: {
+    padding: 4,
+  },
+}));
+
+const CellContainer = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+  },
+}));
+
+const CellLabel = styled(Typography)(({ theme }) => ({
+  paddingTop: 8,
+  display: "inline-block",
+  marginRight: 8,
+
+  [theme.breakpoints.down("sm")]: {
+    textAlign: "center",
+    marginRight: 0,
+  },
+}));
+
+const CellField = styled(TextField)(({ theme }) => ({
+  minWidth: 40,
+  [theme.breakpoints.down("sm")]: {
+    maxWidth: 70,
+  },
+}));
+
+const CellField2 = styled(OutlinedInput)({
+  minWidth: 40,
+  "& input::-webkit-outer-spin-button": {
+    display: "none",
+  },
+  "& input::-webkit-inner-spin-button": {
+    display: "none",
+  },
+  "& input[type=number]": {
+    MozAppearance: "textfield",
+  },
+});
+
+export {
+  ModuleContainer,
+  Header,
+  ControlButtonsContainer,
+  ControlButtions,
+  DescriptionContainer,
+  CustomIconButton,
+  CustomCell,
+  CustomCellButton,
+  CellContainer,
+  CellLabel,
+  CellField,
+  CellField2,
+}
--- src/bucket-sim/edit.tsx
+++ src/bucket-sim/edit.tsx
@@ -10,14 +10,10 @@ import {
   Button,
   FormControl,
   FormHelperText,
-  IconButton,
   InputAdornment,
-  OutlinedInput,
   Paper,
-  styled,
   Table,
   TableBody,
-  TableCell,
   TableContainer,
   TableRow,
   TextField,
@@ -34,8 +30,24 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 import { GlobalLoaderContext } from "@components";
+
 import { Form, resolver } from "./validation";
 import { Item } from "./types";
+
+import {
+  ModuleContainer,
+  Header,
+  ControlButtonsContainer,
+  ControlButtions,
+  DescriptionContainer,
+  CustomIconButton,
+  CustomCell,
+  CustomCellButton,
+  CellContainer,
+  CellLabel,
+  CellField,
+  CellField2,
+} from "./_components";
 
 type Props = {
   matches: {
@@ -44,100 +56,6 @@ type Props = {
 };
 
 const TB = 1000169533440;
-
-const ModuleContainer = styled(Box)({
-  paddingTop: 24,
-  paddingBottom: 24,
-});
-
-const Header = styled(Box)({
-  display: "flex",
-  marginBottom: 32,
-});
-
-const ControlButtonsContainer = styled(Box)(({ theme }) => ({
-  display: "flex",
-  flexDirection: "row",
-  alignItems: "center",
-
-  [theme.breakpoints.down("sm")]: {
-    flexDirection: "column-reverse",
-    alignItems: "unset",
-  },
-}));
-
-const ControlButtions = styled(Button)(({ theme }) => ({
-  minWidth: 120,
-  marginLeft: 16,
-  [theme.breakpoints.down("sm")]: {
-    marginTop: 8,
-  },
-}));
-
-const DescriptionContainer = styled(Paper)({
-  marginBottom: 16,
-});
-
-const CustomIconButton = styled(IconButton)({
-  width: 32,
-  height: 32,
-});
-
-const CustomCell = styled(TableCell)(({ theme }) => ({
-  verticalAlign: "top",
-
-  [theme.breakpoints.down("sm")]: {
-    padding: 4,
-  },
-}));
-
-const CustomCellButton = styled(TableCell)(({ theme }) => ({
-  verticalAlign: "center",
-
-  [theme.breakpoints.down("sm")]: {
-    padding: 4,
-  },
-}));
-
-const CellContainer = styled(Box)(({ theme }) => ({
-  display: "flex",
-  flexDirection: "row",
-
-  [theme.breakpoints.down("sm")]: {
-    flexDirection: "column",
-  },
-}));
-
-const CellLabel = styled(Typography)(({ theme }) => ({
-  paddingTop: 8,
-  display: "inline-block",
-  marginRight: 8,
-
-  [theme.breakpoints.down("sm")]: {
-    textAlign: "center",
-    marginRight: 0,
-  },
-}));
-
-const CellField = styled(TextField)(({ theme }) => ({
-  minWidth: 40,
-  [theme.breakpoints.down("sm")]: {
-    maxWidth: 70,
-  },
-}));
-
-const CellField2 = styled(OutlinedInput)({
-  minWidth: 40,
-  "& input::-webkit-outer-spin-button": {
-    display: "none",
-  },
-  "& input::-webkit-inner-spin-button": {
-    display: "none",
-  },
-  "& input[type=number]": {
-    MozAppearance: "textfield",
-  },
-});
 
 const BucketSimEdit = (props: Props) => {
   const { isLoading, toggleLoader } = useContext(GlobalLoaderContext);
