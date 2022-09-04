dae65fdb85a2859f8373c806507cb545a127e19d
Rin Minase <rin.black.raison.detre@gmail.com>
2022-09-04 19:20:50 +0800

    ✨ feat: added common dropdown component

---
+++ src/common/ControlledSelect.tsx
@@ -0,0 +1,86 @@
+import { Control, Controller } from "react-hook-form";
+import { v4 as uuid } from "uuid";
+
+import {
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
+
+export type OptionsProps =
+  | string[]
+  | Array<{
+      label: string;
+      key?: any;
+      value?: number | string;
+    }>;
+
+type Props = {
+  control: Control<any>;
+  name: string;
+  options?:
+    | string[]
+    | Array<{
+        label: string;
+        key?: any;
+        value?: number | string;
+      }>;
+  variant?: "outlined" | "standard" | "filled";
+  label?: string;
+  helperText?: string;
+  error?: boolean;
+  disabled?: boolean;
+  displayEmpty?: boolean;
+};
+
+const id = uuid();
+const ControlledSelect = (props: Props) => {
+  return (
+    <Controller
+      name={props.name}
+      control={props.control}
+      render={({ field: { onChange, value } }) => (
+        <FormControl>
+          <InputLabel id={id}>{props.label}</InputLabel>
+          <Select
+            id={id}
+            variant={props.variant ?? "outlined"}
+            onChange={onChange}
+            label={props.label}
+            error={props.error}
+            disabled={props.disabled}
+            displayEmpty={props.displayEmpty}
+            value={value}
+          >
+            {props.options &&
+              props.options.map((item) => {
+                if (typeof item === "object") {
+                  return (
+                    <MenuItem
+                      value={item.value ?? item.label}
+                      key={item.key ?? item.label}
+                    >
+                      {item.label}
+                    </MenuItem>
+                  );
+                }
+
+                return (
+                  <MenuItem value={item} key={`dropdown-${item}`}>
+                    {item}
+                  </MenuItem>
+                );
+              })}
+          </Select>
+          {props.error && (
+            <FormHelperText error>{props.helperText}</FormHelperText>
+          )}
+        </FormControl>
+      )}
+    />
+  );
+};
+
+export default ControlledSelect;
--- src/common/index.ts
+++ src/common/index.ts
@@ -1,9 +1,11 @@
 export type { AlertProps } from "./Alert";
+export type { OptionsProps } from "./ControlledSelect";
 export type { Qualities } from "./Quality";
 
 export { default as Alert } from "./Alert";
 export { default as ControlledField } from "./ControlledField";
 export { default as ControlledDatepicker } from "./ControlledDatepicker";
+export { default as ControlledSelect } from "./ControlledSelect";
 export { default as DashboardTile } from "./DashboardTile";
 export { default as Nav } from "./Nav";
 export { default as NavCommon } from "./NavCommon";
