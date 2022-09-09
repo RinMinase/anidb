727b6ef689ed8493b82a5ebfa8f4d0233f95ec49
Rin Minase <rin.black.raison.detre@gmail.com>
2022-09-10 02:55:34 +0800

    💎 style: removed unused code

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,19 +1,5 @@
 /// <reference types="vite/client" />
 
-// export type Fragment = preact.JSX.Element
-
-// namespace preact {
-//   interface Component {
-//     // This is a workaround for
-//     // https://github.com/preactjs/preact/issues/1206
-//     refs: Record<string, any>;
-//   }
-// }
-
-// declare module "preact" {
-//   export = preact;
-// }
-
 interface ImportMetaEnv {
   readonly VITE_API_URL: string
   readonly VITE_API_KEY: string
