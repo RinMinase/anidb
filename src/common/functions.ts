941405979a2ce5f13120bf1091ad55419ff02144
Rin Minase <rin.black.raison.detre@gmail.com>
2022-11-05 00:34:51 +0800

    ✨ feat: added waiting for element util function

--- src/common/functions.ts
+++ src/common/functions.ts
@@ -1,7 +1,27 @@
 export function emptyStringToNull(value: any, originalValue: any) {
-  if (typeof originalValue === 'string' && originalValue === '') {
+  if (typeof originalValue === "string" && originalValue === "") {
     return null;
   }
 
   return value;
+}
+
+export async function waitForElement(selector: string): Promise<any> {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver(() => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
 }
