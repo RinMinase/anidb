9a77849a1603b1654cc6117451eb60ab502df3aa
Rin Minase <rin.black.raison.detre@gmail.com>
2024-09-06 16:39:37 +0800

    ✨ feat: modified layout of quality component

--- src/common/components/Quality.tsx
+++ src/common/components/Quality.tsx
@@ -1,4 +1,4 @@
-import { styled, Tooltip } from "@mui/material";
+import { styled } from "@mui/material";
 
 export type Qualities =
   | "4K 2160p"
@@ -12,27 +12,45 @@ type Props = {
 };
 
 const Container = styled("div")<Props>(({ quality }) => {
-  let bgcolor = "#777";
+  let bgcolor = "#666";
+  let color = "#fff";
 
-  if (quality === "4K 2160p") bgcolor = "#f9c";
-  if (quality === "FHD 1080p") bgcolor = "#9f9";
-  if (quality === "HD 720p") bgcolor = "#9cf";
-  if (quality === "HQ 480p") bgcolor = "#fc6";
+  if (quality === "4K 2160p") {
+    bgcolor = "#f9c";
+    color = "#444";
+  }
+  if (quality === "FHD 1080p") {
+    bgcolor = "#9f9";
+    color = "#444";
+  }
+  if (quality === "HD 720p") {
+    bgcolor = "#9cf";
+    color = "#444";
+  }
+  if (quality === "HQ 480p") {
+    bgcolor = "#fc6";
+    color = "#444";
+  }
 
   return {
-    width: 10,
-    height: 10,
+    width: 30,
     display: "inline-block",
-    marginRight: 8,
+    marginRight: 12,
     backgroundColor: bgcolor,
+    fontSize: 10,
+    borderRadius: 4,
+    paddingTop: 1,
+    paddingBottom: 1,
+    textAlign: "center",
+    color,
   };
 });
 
 const Quality = (props: Props) => {
   return (
-    <Tooltip title={props.quality} placement="top">
-      <Container quality={props.quality} />
-    </Tooltip>
+    <Container quality={props.quality}>
+      {props.quality ? props.quality.split(" ")[0] : ""}
+    </Container>
   );
 };
 
