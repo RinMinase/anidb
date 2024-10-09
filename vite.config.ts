a43a9587f1e9d44ff27071d5e3dc0aef3b9ca497
Rin Minase <rin.black.raison.detre@gmail.com>
2024-10-09 16:21:30 +0800

    ✨ feat: temporary removal of compression plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig, PluginOption } from "vite";
 
 import preact from "@preact/preset-vite";
-
 import tsconfigPaths from "vite-tsconfig-paths";
-import viteCompression from "vite-plugin-compression";
 import eslint from "vite-plugin-eslint";
 import svgr from "vite-plugin-svgr";
 
@@ -19,31 +17,8 @@ const eslintConfig: PluginOption = {
   enforce: "post",
 };
 
-const compressionConfig: PluginOption = {
-  ...viteCompression({
-    ext: ".jgz",
-    verbose: false,
-  }),
-  apply: "build",
-};
-
-const compressionConfigBrotli: PluginOption = {
-  ...viteCompression({
-    algorithm: "brotliCompress",
-    verbose: false,
-  }),
-  apply: "build",
-};
-
 export default defineConfig({
-  plugins: [
-    eslintConfig,
-    compressionConfig,
-    compressionConfigBrotli,
-    preact(),
-    svgr(),
-    tsconfigPaths(),
-  ],
+  plugins: [eslintConfig, preact(), svgr(), tsconfigPaths()],
   server: {
     port: 3000,
     host: true,
