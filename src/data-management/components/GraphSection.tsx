f1b1afc745350ddb6c2bd9b6642d94cb1937e19a
Rin Minase <rin.black.raison.detre@gmail.com>
2025-02-01 02:43:55 +0800

    ✨ feat(management): appended new charts

--- src/data-management/components/GraphSection.tsx
+++ src/data-management/components/GraphSection.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "preact/hooks";
 import { Chart, ChartOptions, registerables } from "chart.js";
 import ChartDataLabels from "chartjs-plugin-datalabels";
 import { Box, Grid2 as Grid, styled, Typography } from "@mui/material";
+import { grey, red, orange, indigo, green } from "@mui/material/colors";
 
 import { Graph } from "../types";
 
@@ -10,14 +11,17 @@ type Props = {
 };
 
 let chartQuality: Chart;
+let chartRatings: Chart;
 let chartMonth: Chart;
+let chartYear: Chart;
+
 Chart.register(...registerables, ChartDataLabels);
 
 const GraphTopContainer = styled(Box)({
   paddingTop: 24,
 });
 
-const PieChartContainer = styled(Box)({
+const BarChartContainer = styled(Box)({
   width: "100%",
   maxWidth: 450,
   textAlign: "center",
@@ -26,6 +30,12 @@ const PieChartContainer = styled(Box)({
 const LineChartContainer = styled(Box)({
   width: "100%",
   maxWidth: 600,
+  textAlign: "center",
+});
+
+const PieChartContainer = styled(Box)({
+  width: "100%",
+  maxWidth: 450,
   textAlign: "center",
 });
 
@@ -110,6 +120,81 @@ const GraphSection = (props: Props) => {
     },
   };
 
+  const chartRatingsInitialData = {
+    labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+    datasets: [
+      {
+        data: [],
+        // [gray500, ]
+        backgroundColor: [
+          grey[500],
+          red[800],
+          red[500],
+          red[300],
+          orange[800],
+          orange[500],
+          orange[300],
+          indigo[400],
+          indigo[800],
+          green[500],
+          green[800],
+        ],
+      },
+    ],
+  };
+
+  const chartRatingsOptions: ChartOptions = {
+    aspectRatio: 1.5,
+    hover: { mode: null as any },
+    scales: {
+      y: {
+        grace: "20%",
+      },
+    },
+    plugins: {
+      datalabels: {
+        anchor: "end",
+        align: "top",
+        clamp: true,
+        color: "#2196F3",
+      },
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: false,
+      },
+    },
+  };
+
+  const chartYearInitialData = {
+    labels: [],
+    datasets: [{ data: [] }],
+  };
+
+  const chartYearOptions: ChartOptions = {
+    aspectRatio: 1.5,
+    scales: {
+      y: {
+        grace: "20%",
+      },
+    },
+    plugins: {
+      datalabels: {
+        anchor: "end",
+        align: "top",
+        clamp: true,
+        color: "#2196F3",
+      },
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: false,
+      },
+    },
+  };
+
   useEffect(() => {
     if (document.getElementById("quality")) {
       const canvas = document.getElementById("quality") as HTMLCanvasElement;
@@ -123,6 +208,18 @@ const GraphSection = (props: Props) => {
       });
     }
 
+    if (document.getElementById("ratings")) {
+      const canvas = document.getElementById("ratings") as HTMLCanvasElement;
+      const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+      chartRatings = new Chart(ctx, {
+        type: "bar",
+        plugins: [ChartDataLabels],
+        options: chartRatingsOptions,
+        data: chartRatingsInitialData,
+      });
+    }
+
     if (document.getElementById("month")) {
       const canvas = document.getElementById("month") as HTMLCanvasElement;
       const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
@@ -132,6 +229,18 @@ const GraphSection = (props: Props) => {
         plugins: [ChartDataLabels],
         options: chartMonthOptions,
         data: chartMonthInitialData,
+      });
+    }
+
+    if (document.getElementById("year")) {
+      const canvas = document.getElementById("year") as HTMLCanvasElement;
+      const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+      chartYear = new Chart(ctx, {
+        type: "bar",
+        plugins: [ChartDataLabels],
+        options: chartYearOptions,
+        data: chartYearInitialData,
       });
     }
   }, []);
@@ -168,6 +277,24 @@ const GraphSection = (props: Props) => {
     chartMonth.update();
   }, [props.graph.months]);
 
+  useEffect(() => {
+    if (props.graph.ratings.length) {
+      chartRatings.data.datasets[0].data = [...props.graph.ratings];
+      chartRatings.update();
+    }
+  }, [props.graph.ratings]);
+
+  useEffect(() => {
+    if (props.graph.year.length) {
+      props.graph.year.forEach((item) => {
+        chartYear.data.labels?.push(item.year);
+        chartYear.data.datasets[0].data.push(item.value);
+      });
+
+      chartYear.update();
+    }
+  }, [props.graph.year]);
+
   return (
     <GraphTopContainer>
       <Grid container spacing={2}>
@@ -190,15 +317,44 @@ const GraphSection = (props: Props) => {
           display="flex"
           alignItems="center"
           flexDirection="column"
+          gap={2}
+        >
+          <Typography variant="h6" textAlign="center">
+            Titles per Rating
+          </Typography>
+          <BarChartContainer>
+            <canvas id="ratings" />
+          </BarChartContainer>
+        </Grid>
+        <Grid
+          size={{ xs: 12, sm: 12, md: 6 }}
+          display="flex"
+          alignItems="center"
+          flexDirection="column"
           justifyContent="center"
           gap={2}
         >
           <Typography variant="h6" textAlign="center">
-            Titles per Month
+            Titles Watched per Month
           </Typography>
           <LineChartContainer>
             <canvas id="month" />
           </LineChartContainer>
+        </Grid>
+        <Grid
+          size={{ xs: 12, sm: 12, md: 6 }}
+          display="flex"
+          alignItems="center"
+          flexDirection="column"
+          justifyContent="center"
+          gap={2}
+        >
+          <Typography variant="h6" textAlign="center">
+            Titles Watched per Year
+          </Typography>
+          <BarChartContainer>
+            <canvas id="year" />
+          </BarChartContainer>
         </Grid>
       </Grid>
     </GraphTopContainer>
