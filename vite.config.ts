88ec19d10fdb0b3aa952e67c368e50f9989712ca
Rin Minase <rin.black.raison.detre@gmail.com>
2026-01-27 02:22:58 +0800

    ✨ feat: added dev only plugins

--- vite.config.ts
+++ vite.config.ts
@@ -4,23 +4,32 @@ import preact from "@preact/preset-vite";
 import svgr from "vite-plugin-svgr";
 import biome from 'vite-plugin-biome';
 
-export default defineConfig({
-  plugins: [
-    preact(),
-    svgr(),
+export default defineConfig(({ command }) => {
+
+  // Dev Plugins
+  const isDev = command === 'serve'
+  const devPlugins = [
     biome({ mode: "check", failOnError: false }),
-  ],
-  server: {
-    port: 3000,
-    host: true,
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-      "@components": "/src/common",
+  ]
+
+  return {
+    plugins: [
+      preact(),
+      svgr(),
+      ...(isDev ? devPlugins : []),
+    ],
+    server: {
+      port: 3000,
+      host: true,
     },
-  },
-  build: {
-    chunkSizeWarningLimit: 1000,
-  },
+    resolve: {
+      alias: {
+        "@": "/src",
+        "@components": "/src/common",
+      },
+    },
+    build: {
+      chunkSizeWarningLimit: 1000,
+    },
+  }
 });
